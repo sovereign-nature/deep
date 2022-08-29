38cf816954d34b42cee25ffa750cf9f549447dbc
Vadim Smakhtin <vadim@sovereignnature.com>
2022-08-29 18:20:35 +0400

    Added eslint for tailwind

--- apps/explorer/pages/search.vue
+++ apps/explorer/pages/search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lg:px-24">
     <SNIFilter />
-    <SNITableMobile class="lg:hidden mx-6 py-12" />
-    <SNITable class="hidden lg:block lg:w-full lg:h-screen" />
+    <SNITableMobile class="mx-6 py-12 lg:hidden" />
+    <SNITable class="hidden lg:block lg:h-screen lg:w-full" />
   </div>
 </template>
--- packages/eslint-config-sni-vue/index.js
+++ packages/eslint-config-sni-vue/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  extends: ['plugin:vue/vue3-essential']
+  extends: ['plugin:vue/vue3-essential', 'plugin:tailwindcss/recommended'],
+  plugins: ['tailwindcss']
 }
