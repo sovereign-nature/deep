f787ff20565dffcf38a3fa56c8f7387df3df789e
Giovanne Guerra <giovanneguerra@hotmail.com>
2022-09-08 10:43:58 +0100

    Feature/table refactor (#28)
    * chore: Change prettier bracketSpacing
    
    * refactor: Componentize search table
    
    * refactor: Create dynamic data
    
    * refactor: Create dynamic table
    
    * refactor: Remove unecessary imports

--- apps/explorer/components/SNITable.vue
+++ apps/explorer/components/SNITable.vue
@@ -1,45 +1,56 @@
 <template>
-  <div class="flex w-full justify-center">
-    <div class="w-full overflow-x-auto">
-      <table class="table w-full">
-        <!-- head -->
-        <thead>
-          <tr class="border-b-2 border-primary bg-neutral text-white">
-            <th class="bg-inherit font-light text-inherit">Id</th>
-            <th class="bg-inherit font-light text-inherit">Name</th>
-            <th class="bg-inherit font-light text-inherit">Location</th>
-            <th class="bg-inherit font-light text-inherit">Created</th>
-            <th class="bg-inherit font-light text-inherit">Updated</th>
-            <th class="bg-inherit font-light text-inherit">
-              Conservation status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row 1 -->
-          <tr class="border-b-2 border-primary bg-neutral text-white">
-            <td class="bg-inherit font-light">001</td>
-            <td class="bg-inherit font-light">Leo Sabotus</td>
-            <td class="bg-inherit font-light">
-              <span class="rounded-md border-2 p-2">Preserved Specimen</span>
-            </td>
-            <td class="bg-inherit font-light">01-01-2022</td>
-            <td class="bg-inherit font-light">12-12-2022</td>
-            <td class="bg-inherit font-light">Kenya</td>
-          </tr>
-          <!-- row 2 -->
-          <tr class="border-primary bg-neutral text-white">
-            <td class="bg-inherit font-light">002</td>
-            <td class="bg-inherit font-light">Leo Supremus</td>
-            <td class="bg-inherit font-light">
-              <span class="rounded-md border-2 p-2">Preserved Specimen</span>
-            </td>
-            <td class="bg-inherit font-light">01-01-2022</td>
-            <td class="bg-inherit font-light">12-12-2022</td>
-            <td class="bg-inherit font-light">Kenya</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  <div class="w-full border-collapse overflow-x-auto rounded-lg">
+    <TableHeader></TableHeader>
+    <TableRow :lions="lions"></TableRow>
   </div>
 </template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      lions: [
+        {
+          id: '001',
+          name: 'Leo Sabotus',
+          conservationStatus: 'Preserved Specimen',
+          created: '01-01-2022',
+          updated: '12-12-2022',
+          location: 'Kenya'
+        },
+        {
+          id: '002',
+          name: 'Leo Leonus',
+          conservationStatus: 'Human Observation',
+          created: '01-01-2022',
+          updated: '12-12-2022',
+          location: 'Kenya'
+        },
+        {
+          id: '003',
+          name: 'Leo Fasciculus',
+          conservationStatus: 'Machine Observation',
+          created: '01-01-2022',
+          updated: '12-12-2022',
+          location: 'Kenya'
+        },
+        {
+          id: '004',
+          name: 'Leo Tiranus',
+          conservationStatus: 'Preserved Specimen',
+          created: '01-01-2022',
+          updated: '12-12-2022',
+          location: 'Kenya'
+        },
+        {
+          id: '005',
+          name: 'Leo Aegiae',
+          conservationStatus: 'Preserved Specimen',
+          created: '01-01-2022',
+          updated: '12-12-2022',
+          location: 'Kenya'
+        }
+      ]
+    }
+  }
+}
+</script>
