c02cea094a084d1dae817e64e25faaf5aa4dc59e
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-09-08 17:34:00 +0100

    refactor: Add link to search

--- apps/explorer/components/TableHeader.vue
+++ apps/explorer/components/TableHeader.vue
@@ -3,23 +3,19 @@
     class="grid grid-flow-col auto-cols-fr place-items-center border-b-2 border-primary bg-neutral py-3 text-white"
   >
     <div
-      v-for="(value, key) in data[0]"
+      v-for="(value, key) in props.data[0]"
       :key="key"
       class="bg-inherit font-light text-inherit"
     >
-      {{ splitCamelCase(key) }}
+      {{ splitCamelCase(key.toString()) }}
     </div>
   </div>
 </template>
-<script lang="ts">
-export default {
-  props: ['data'],
-  methods: {
-    splitCamelCase: function (text: string): string {
-      return (
-        text.charAt(0).toUpperCase() + text.slice(1).replace(/[A-Z]/g, ' $&')
-      )
-    }
-  }
+<script setup lang="ts">
+const props = defineProps({
+  data: []
+})
+const splitCamelCase = function (text: string): string {
+  return text.charAt(0).toUpperCase() + text.slice(1).replace(/[A-Z]/g, ' $&')
 }
 </script>
