cf64d8c5b1e2212a2bb4fc7a46f7a7d8b4a597b2
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-09-22 15:24:03 +0100

    refactor: Change soul properties

--- apps/explorer/types/soul.ts
+++ apps/explorer/types/soul.ts
@@ -1,8 +1,16 @@
+import SoulProperty from '~/types/soul-property'
+
 export interface Soul {
   id: string
-  name: string
-  conservationStatus: string
-  created: string
-  updated: string
-  location: string
+  status: number,
+  createdTimestamp: number,
+  updatedTimestamp: number,
+  owner: string,
+  name: string,
+  description: string,
+  image: string,
+  properties: SoulProperty
+  symbol: string,
+  collectionName: string,
+  oracle: string
 }
