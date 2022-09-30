28e716593a338b107895c445103dfac9c8f2cfac
Giovanne Guerra <giovanne@sovereignnature.com>
2022-09-30 15:26:47 +0100

    Bugfix/display map (#104)
    * refactor: Display map and refactor properties
    
    * refactor: Change mapbox plugin
    
    * feature. Make popup works
    
    * Changed build to vercel server
    
    * chore: Remove unused dependency
    
    * chore: Solve dependencies issues
    
    * refactor. Remove popup and image marker
    
    * chore: Change env var
    
    * Updated explorer config
    
    Co-authored-by: Vadim Smakhtin <vadim@sovereignnature.com>

--- apps/explorer/components/SNIProperties.vue
+++ apps/explorer/components/SNIProperties.vue
@@ -3,14 +3,16 @@
     <h1 class="text-3xl">Properties</h1>
     <div class="my-6 flex w-full">
       <div
-        v-for="(value, key) in props.properties"
+        v-for="(value, key) in properties"
         :key="key"
         class="mr-2 rounded-xl border border-primary-content bg-primary py-2 px-4 text-center"
       >
         <p class="text-sm text-primary-content">
           {{ splitCamelCase(key.toString()) }}
         </p>
-        <p class="mt-1 text-xl text-white">{{ value }}</p>
+        <p class="mt-1 text-xl text-white">
+          {{ value }}
+        </p>
       </div>
     </div>
   </div>
@@ -19,7 +21,7 @@
 import { SoulProperty } from '~~/types/soul-property'
 import { splitCamelCase } from '~/utils/index'
 
-const props = defineProps({
+defineProps({
   properties: {
     type: Object as () => SoulProperty,
     required: false,
