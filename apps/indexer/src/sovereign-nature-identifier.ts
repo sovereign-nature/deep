11b6f515debb864d98d579e41e012b4ce9f54c55
Vadim Smakhtin <vadim@sovereignnature.com>
2022-09-30 16:07:14 +0200

    Code cleanup

--- apps/indexer/src/sovereign-nature-identifier.ts
+++ apps/indexer/src/sovereign-nature-identifier.ts
@@ -17,14 +17,18 @@ const CONTRACT = Address.fromString(
   '0xB72a77d425aad2faAE3F695846b337E7d65D098e'
 )
 
-function findEntity(id: string): SNI {
+// eslint-disable-next-line @typescript-eslint/ban-types
+function findEntity(id: string, blockTimestamp: BigInt): SNI {
   let entity = SNI.load(id)
 
   if (!entity) {
     entity = new SNI(id)
+    entity.createdAt = blockTimestamp
 
-    // entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.fromI32(0)
   }
+
+  entity.updatedAt = blockTimestamp
 
   // BigInt and BigDecimal math are supported
   // @ts-ignore
@@ -33,47 +37,9 @@ function findEntity(id: string): SNI {
   return entity
 }
 
-function test(a: number): number {
-  return a + 1
-}
-
+//TODO: Decide if we need approval events. Marketplace integration is not planned.
 export function handleApproval(event: Approval): void {
   console.log(event.address.toString())
-  // const id = event.transaction.from.toHex()
-  // const entity = findEntity(id)
-  // // Entity fields can be set based on event parameters
-  // entity.owner = event.params.owner
-  // entity.approved = event.params.approved
-  // // Entities can be written to the store with `.save()`
-  // entity.save()
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.MINTER_ROLE(...)
-  // - contract.ORACLE_ROLE(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.hasRole(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.statusOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
 }
 
 export function handleApprovalForAll(event: ApprovalForAll): void {
@@ -100,7 +66,7 @@ export function handleTokenURISet(event: TokenURISet): void {
   const tokenId = event.params.tokenId
   const tokenURI = event.params.tokenURI
 
-  const entity = findEntity(tokenId.toHex())
+  const entity = findEntity(tokenId.toHex(), event.block.timestamp)
   entity.tokenURI = tokenURI
 
   entity.save()
@@ -116,7 +82,7 @@ export function handleTransfer(event: Transfer): void {
 
   const timestamp = event.block.timestamp
 
-  const entity = new SNI(tokenId.toHex())
+  const entity = findEntity(tokenId.toHex(), event.block.timestamp)
   entity.owner = owner
   entity.status = status
   entity.tokenURI = tokenURI
