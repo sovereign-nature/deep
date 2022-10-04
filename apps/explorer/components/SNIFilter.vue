0ae154d39760bb4fc4c6fedf11ae60ba2053ecb4
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-10-04 21:34:33 +0100

    feature: Implement VueDatepicker

--- apps/explorer/components/SNIFilter.vue
+++ apps/explorer/components/SNIFilter.vue
@@ -83,6 +83,18 @@
                 placeholder="Created at"
               />
             </div>
+            <Datepicker
+              v-model="createdDate"
+              dark
+              placeholder="Created at"
+              class="relative inset-y-0 left-0 flex items-center rounded-full bg-inherit p-3"
+            />
+            <Datepicker
+              v-model="updatedDate"
+              dark
+              placeholder="Updated at"
+              class="relative inset-y-0 left-0 flex items-center rounded-full bg-inherit p-3"
+            />
             <div
               class="relative rounded-full border-2 border-accent bg-neutral p-2"
             >
@@ -160,57 +172,26 @@
           <option>Kenya</option>
           <option>Netherlands</option>
         </select>
-        <div class="relative w-1/2 rounded-full bg-neutral">
-          <div
-            class="pointer-events-none absolute inset-y-0 left-0 flex items-center rounded-full bg-inherit p-3"
-          >
-            <svg
-              aria-hidden="true"
-              class="h-5 w-5 text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </div>
-          <input
-            datepicker
-            type="text"
-            class="block h-full w-full rounded-full bg-inherit pl-12 text-sm text-white placeholder:text-white focus:outline-none"
-            placeholder="Created at"
-          />
-        </div>
-        <div class="relative w-1/2 rounded-full bg-neutral">
-          <div
-            class="pointer-events-none absolute inset-y-0 left-0 flex items-center rounded-full bg-inherit p-3"
-          >
-            <svg
-              aria-hidden="true"
-              class="h-5 w-5 text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </div>
-          <input
-            datepicker
-            type="text"
-            class="block h-full w-full rounded-full bg-inherit pl-12 text-sm text-white placeholder:text-white focus:outline-none"
-            placeholder="Updated at"
-          />
-        </div>
+        <Datepicker
+          v-model="createdDate"
+          dark
+          placeholder="Created at"
+          class="relative inset-y-0 left-0 flex items-center rounded-full bg-inherit p-3"
+        />
+
+        <Datepicker
+          v-model="updatedDate"
+          dark
+          placeholder="Updated at"
+          class="relative inset-y-0 left-0 flex items-center rounded-full bg-inherit p-3"
+        />
       </div>
     </form>
   </div>
 </template>
+<script lang="ts" setup>
+import Datepicker from '@vuepic/vue-datepicker'
+
+const createdDate = ref([])
+const updatedDate = ref([])
+</script>
--- apps/explorer/pages/search.vue
+++ apps/explorer/pages/search.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="lg:px-24 lg:pb-12">
-      <!-- <SNIFilter /> -->
+      <SNIFilter></SNIFilter>
       <SNITableMobile
         :data="(details as Soul[])"
         class="mx-6 py-12 lg:hidden"
@@ -25,3 +25,28 @@ if (error.value) {
   console.error(error.value)
 }
 </script>
+<style lang="scss">
+@import '@vuepic/vue-datepicker/src/VueDatePicker/style/main.scss';
+// datepicker customization
+.dp__theme_dark {
+  --dp-background-color: #5d5656;
+  --dp-text-color: #ffffff;
+  --dp-hover-color: #484848;
+  --dp-hover-text-color: #ffffff;
+  --dp-hover-icon-color: #959595;
+  --dp-primary-color: #5c7f67;
+  --dp-primary-text-color: #ffffff;
+  --dp-secondary-color: #ecf4e7;
+  --dp-border-color: #2d2d2d;
+  --dp-menu-border-color: #2d2d2d;
+  --dp-border-color-hover: #ffffff;
+  --dp-disabled-color: #737373;
+  --dp-scroll-bar-background: #212121;
+  --dp-scroll-bar-color: #484848;
+  --dp-success-color: #aae6b9;
+  --dp-success-color-disabled: #5c7f67;
+  --dp-icon-color: #ffffff;
+  --dp-danger-color: #e53935;
+  --dp-highlight-color: rgba(0, 92, 178, 0.2);
+}
+</style>
