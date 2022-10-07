b948803f04a9a40cb204cc3308de8d5cc1fbd90f
Giovanne Guerra <giovanne@sovereignnature.com>
2022-10-07 09:31:03 +0100

    Feature/implement datepicker (#118)
    * feature: Implement VueDatepicker
    
    * Style: Improve the style of date picker
    
    * refactor: Split filter component into responsive

--- apps/explorer/pages/search.vue
+++ apps/explorer/pages/search.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="lg:px-24 lg:pb-12">
-      <!-- <SNIFilter /> -->
+    <div class="lg:px-24">
+      <SNIFilter></SNIFilter>
       <SNITableMobile
         :data="(details as Soul[])"
         class="mx-6 py-12 lg:hidden"
@@ -25,3 +25,43 @@ if (error.value) {
   console.error(error.value)
 }
 </script>
+<style lang="scss">
+@import '@vuepic/vue-datepicker/src/VueDatePicker/style/main.scss';
+// datepicker customization
+.dp__theme_dark {
+  --dp-background-color: #5d5656;
+  --dp-text-color: #ffffff;
+  --dp-hover-color: #484848;
+  --dp-hover-text-color: #ffffff;
+  --dp-hover-icon-color: #959595;
+  --dp-primary-color: #5c7f67;
+  --dp-primary-text-color: #ffffff;
+  --dp-secondary-color: #ecf4e7;
+  --dp-border-color: #2d2d2d;
+  --dp-menu-border-color: #2d2d2d;
+  --dp-border-color-hover: #ffffff;
+  --dp-disabled-color: #737373;
+  --dp-scroll-bar-background: #212121;
+  --dp-scroll-bar-color: #484848;
+  --dp-success-color: #aae6b9;
+  --dp-success-color-disabled: #5c7f67;
+  --dp-icon-color: #ffffff;
+  --dp-danger-color: #e53935;
+  --dp-highlight-color: rgba(0, 92, 178, 0.2);
+
+  .dp__input_wrap {
+    .dp__input {
+      border-radius: 50px;
+      border: none;
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      &::placeholder {
+        color: #ffffff;
+        opacity: 1;
+        font-weight: 600;
+        font-size: 0.875rem;
+      }
+    }
+  }
+}
+</style>
