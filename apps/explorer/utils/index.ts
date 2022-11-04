4f1e701c7b9fcbaa23a2e1defe3fc58374502570
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-11-04 16:11:53 +0000

    feature: Create truncate function

--- apps/explorer/utils/index.ts
+++ apps/explorer/utils/index.ts
@@ -1,3 +1,9 @@
 export function splitCamelCase(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1).replace(/[A-Z]/g, ' $&')
 }
+
+export function truncate(value, charLength): string {
+  return value.toString().length > charLength
+    ? value.toString().substring(0, charLength) + '...'
+    : value
+}
