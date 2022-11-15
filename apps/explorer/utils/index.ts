714695c460645f6ed2917cbb6b8f1e7afcc97b15
giovanneguerra <giovanne@sovereignnature.com>
2022-11-15 16:23:09 +0000

    refactor: Move ipfsToUrl function to utils

--- apps/explorer/utils/index.ts
+++ apps/explorer/utils/index.ts
@@ -2,7 +2,7 @@ export function splitCamelCase(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1).replace(/[A-Z]/g, ' $&')
 }
 
-export function truncate(value, charLength): string {
+export function truncate(value: any, charLength: number): string {
   if (typeof value === 'object') {
     return value
   } else {
@@ -10,4 +10,7 @@ export function truncate(value, charLength): string {
       ? value.toString().substring(0, charLength) + '...'
       : value
   }
+}
+export function ipfsToUrl(address: string): string {
+  return `https://ipfs.io/ipfs/${address.substring(7)}`
 }
