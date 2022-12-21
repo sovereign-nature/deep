4d27c6db83d2aeb4ac9dc1d9918a0c50bcf3c0e4
giovanneguerra <giovanne@sovereignnature.com>
2022-12-21 17:05:33 +0000

    feature: implement getSoulDetailsById function

--- apps/explorer/angular/src/app/models/soul.ts
+++ apps/explorer/angular/src/app/models/soul.ts
@@ -1,9 +1,19 @@
 export interface Soul {
   id: string;
   owner: string;
-  status: string;
-  createdAt: string;
-  updatedAt: string;
+  status: number;
+  createdAt: number;
+  updatedAt: number;
   name: string;
   collectionName: string;
+  description?: string;
+  image?: string;
+  symbol?: string;
+  oracle?: string;
+  statusDescription?: string;
+  taxonId?: string;
+  tokenId?: string;
+  tokenURI?: string;
+  geometry?: string;
+  conservationStatus?: number;
 }
