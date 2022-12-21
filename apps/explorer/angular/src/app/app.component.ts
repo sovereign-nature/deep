fcaf8d74166ed1b7a71316e989e212e66574096f
giovanneguerra <giovanne@sovereignnature.com>
2022-12-21 13:18:28 +0000

    feature: Add gql-apollo on angular

--- apps/explorer/angular/src/app/app.component.ts
+++ apps/explorer/angular/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
+const SOULS_LIST = gql`
+  query sniList {
+    snis {
+      id
+      owner
+      status
+      createdAt
+      updatedAt
+      name
+      collectionName
+    }
+  }
+`;
+
+interface SNI {
+  snis: Soul[];
+}
+
+interface Soul {
+  id: string;
+  owner: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  collectionName: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+  souls$!: Observable<Soul[]>;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.souls$ = this.apollo
+      .watchQuery<SNI>({
+        query: SOULS_LIST,
+      })
+      .valueChanges.pipe(
+        map((val) => val.data.snis),
+        shareReplay()
+      );
+  }
 }
