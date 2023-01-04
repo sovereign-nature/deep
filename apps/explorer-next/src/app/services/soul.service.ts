3d49a310fb723f98edacdffce4d20c199520fcf6
giovanneguerra <giovanne@sovereignnature.com>
2023-01-04 16:15:42 +0000

    refactor: Improve soul service

--- apps/explorer-next/src/app/services/soul.service.ts
+++ apps/explorer-next/src/app/services/soul.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { Apollo } from 'apollo-angular';
+import { DocumentNode } from 'graphql';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { SNIDetail, SNIList } from '../models/sni';
-import { SOULS_LIST, SOUL_DETAIL } from '../queries/sni';
+import { SNIData, SNIList } from '../models/sni';
+import { Soul, SoulFilter } from '../models/soul';
+import { SOULS_LIST } from '../queries/sni';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SoulService {
-  private subject = new BehaviorSubject<any>({});
-  filteredSouls$: Observable<any> = this.subject.asObservable();
+  private subject = new BehaviorSubject<SoulFilter>({
+    searchById: '',
+    soulStatus: -1,
+    createdDate: 0,
+    updatedDate: 0,
+  });
+  filteredSouls$: Observable<SoulFilter> = this.subject.asObservable();
 
   constructor(private apollo: Apollo) {}
 
-  getSoulsList() {
+  getSoulsList(): Observable<Soul[]> {
     return this.apollo
       .watchQuery<SNIList>({
         query: SOULS_LIST,
@@ -26,22 +32,34 @@ export class SoulService {
       );
   }
 
-  getSoulDetailsById(soulId: string) {
+  getSoulDataById(
+    soulId: string,
+    query: DocumentNode
+  ): Observable<Partial<Soul>> {
     return this.apollo
-      .watchQuery<SNIDetail>({
-        query: SOUL_DETAIL,
+      .watchQuery<SNIData>({
+        query,
         variables: {
           sniId: soulId,
         },
       })
       .valueChanges.pipe(
         map((val) => val?.data?.sni),
-        map(({ __typename, ...val }) => val),
+        map((data) => this.removeTypeName(data)),
         shareReplay()
       );
   }
 
-  setFilterToSouls(data: any) {
+  setFilterToSouls(data: SoulFilter) {
     this.subject.next(data);
+  }
+
+  removeTypeName(data: Soul) {
+    return JSON.parse(
+      JSON.stringify(data, (key, value) => {
+        if (key === '__typename') return;
+        return value;
+      })
+    );
   }
 }
