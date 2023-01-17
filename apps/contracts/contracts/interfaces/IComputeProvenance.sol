042802a55167e11fe627a01be4d9c53db4d14d42
Vadim Smakhtin <vadim@sovereignnature.com>
2023-01-17 10:33:55 +0100

    Added new interfaces for data provenance

---
+++ apps/contracts/contracts/interfaces/IComputeProvenance.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IComputeProvenance {
+    /**
+     * @dev Emitted when new compute script was set.
+     */
+    event ComputeURISet(uint256 indexed tokenId, string computeURI);
+
+    /**
+     * @dev Returns identified object compute script.
+     */
+    function computeURI(uint256 tokenId) external view returns (string memory);
+
+    /**
+     * @dev Sets identified object scompute script.
+     */
+    function setComputeURI(uint256 tokenId, string memory dataURI) external;
+}
