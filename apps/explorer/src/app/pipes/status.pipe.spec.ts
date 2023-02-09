ebe4e0e63618b6d0189bf02f5548b02b95313cf8
giovanneguerra <giovanne@sovereignnature.com>
2023-02-09 12:30:15 +0000

    test: Add correct modules

--- apps/explorer/src/app/pipes/status.pipe.spec.ts
+++ apps/explorer/src/app/pipes/status.pipe.spec.ts
@@ -1,8 +1,14 @@
 import { StatusPipe } from './status.pipe';
 
 describe('StatusPipe', () => {
+  const pipe = new StatusPipe();
   it('create an instance', () => {
-    const pipe = new StatusPipe();
     expect(pipe).toBeTruthy();
+  });
+  it('should return Alive', () => {
+    expect(pipe.transform(0)).toEqual('Alive');
+  });
+  it('should return Dead', () => {
+    expect(pipe.transform(1)).toEqual('Dead');
   });
 });
