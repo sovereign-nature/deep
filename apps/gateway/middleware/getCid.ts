66d73a4f649a93948e8fd7e495eba545e45082d4
Vadim Smakhtin <vadim@sovereignnature.com>
2023-02-13 19:41:48 +0100

    Middleware works

--- apps/gateway/middleware/getCid.ts
+++ apps/gateway/middleware/getCid.ts
@@ -1,11 +1,5 @@
 const getCid = (next) => (root, args, context, info) => {
-  console.log('Middleware executed');
-  // console.log('Args: ', args);
-  // console.log('ROOT:', root);
-  // // console.log('CONTEXT:', context);
-  // console.log('INFO', info);
-
-  // args.cid = args.cid.replace('ipfs://', '');
+  args.cid = args.cid.replace('ipfs://', '');
   return next(root, args, context, info);
 };
 
