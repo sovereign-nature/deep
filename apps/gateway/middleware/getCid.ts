078e2d4386f21a317431e6b4916dcfc983617878
Vadim Smakhtin <vadim@sovereignnature.com>
2023-02-13 19:33:16 +0100

    Transform is not always executed

--- apps/gateway/middleware/getCid.ts
+++ apps/gateway/middleware/getCid.ts
@@ -1,6 +1,11 @@
 const getCid = (next) => (root, args, context, info) => {
-  args.cid = args.cid.replace('ipfs://', '');
-  console.log(args);
+  console.log('Middleware executed');
+  // console.log('Args: ', args);
+  // console.log('ROOT:', root);
+  // // console.log('CONTEXT:', context);
+  // console.log('INFO', info);
+
+  // args.cid = args.cid.replace('ipfs://', '');
   return next(root, args, context, info);
 };
 
