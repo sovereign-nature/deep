8c17b3662eb16c092b6bb0cc6416b8f3123e816d
Vadim Smakhtin <vadim@sovereignnature.com>
2023-02-16 13:07:16 +0100

    Added explorer-sext codebase

---
+++ apps/explorer-sext/components/dashboard/sidebar-element.vue
@@ -0,0 +1,19 @@
+<script setup>
+withDefaults(
+  defineProps({
+    label: String,
+  }),
+  {
+    label: 'label',
+  }
+);
+</script>
+<template>
+  <li>
+    <div
+      class="flex items-center rounded-sm p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-900">
+      <slot></slot>
+      <span class="ml-3">{{ label }}</span>
+    </div>
+  </li>
+</template>
---
+++ apps/explorer-sext/components/icons/gallery.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    aria-hidden="true"
+    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+  </svg>
+</template>
