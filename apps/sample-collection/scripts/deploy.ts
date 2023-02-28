7c1ae2953075cf6f872b62e8429426dc782803e8
Vadim Smakhtin <vadim@sovereignnature.com>
2023-02-28 10:29:43 +0100

    Mint script works

--- apps/sample-collection/scripts/deploy.ts
+++ apps/sample-collection/scripts/deploy.ts
@@ -9,8 +9,6 @@ async function main() {
   console.log('Deployed Sample collection to: ', sample.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
