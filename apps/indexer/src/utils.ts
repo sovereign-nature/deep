41dd092bbefae0277deccac2acde3898071107c6
Vadim Smakhtin <vadim@sovereignnature.com>
2023-02-28 14:35:19 +0100

    Indexer for derrivative contracts (#417)
    * Scaffolded derrivative-indexer project
    
    * Graphql schema update, folder typo fix
    
    * Added transfer event handling
    
    * Tests are working
    
    * Updated deploy command

--- apps/indexer/src/utils.ts
+++ apps/indexer/src/utils.ts
@@ -1,11 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import {
-  BigInt,
-  ipfs,
-  json,
-  JSONValueKind,
-  log,
-} from '@graphprotocol/graph-ts';
+import { BigInt } from '@graphprotocol/graph-ts';
 import { SNI } from '../generated/schema';
 
 // Find entity in database or create a new one if it's not found.
@@ -26,59 +20,59 @@ export function findEntity(tokenId: BigInt, blockTimestamp: BigInt): SNI {
 }
 
 // Fill entity data from IPFS metadata file.
-export function fillFromIPFS(entity: SNI, tokenURI: string): SNI {
-  const data = ipfs.cat(tokenURI.replace('ipfs://', ''));
-  log.info('Reindexing: {}', ['4']);
+// export function fillFromIPFS(entity: SNI, tokenURI: string): SNI {
+//   const data = ipfs.cat(tokenURI.replace('ipfs://', ''));
+//   log.info('Reindexing: {}', ['4']);
 
-  if (data !== null) {
-    const metadata = json.fromBytes(data).toObject();
+//   if (data !== null) {
+//     const metadata = json.fromBytes(data).toObject();
 
-    const name = metadata.get('name');
-    if (name !== null && name.kind == JSONValueKind.STRING) {
-      entity.name = name.toString();
-    }
+//     const name = metadata.get('name');
+//     if (name !== null && name.kind == JSONValueKind.STRING) {
+//       entity.name = name.toString();
+//     }
 
-    const description = metadata.get('description');
-    if (description !== null && description.kind == JSONValueKind.STRING) {
-      entity.description = description.toString();
-    }
+//     const description = metadata.get('description');
+//     if (description !== null && description.kind == JSONValueKind.STRING) {
+//       entity.description = description.toString();
+//     }
 
-    const image = metadata.get('image');
-    if (image !== null && image.kind == JSONValueKind.STRING) {
-      entity.image = image.toString();
-    }
+//     const image = metadata.get('image');
+//     if (image !== null && image.kind == JSONValueKind.STRING) {
+//       entity.image = image.toString();
+//     }
 
-    const properties = metadata.get('properties');
-    if (properties !== null && properties.kind == JSONValueKind.OBJECT) {
-      const propsObject = properties.toObject();
+//     const properties = metadata.get('properties');
+//     if (properties !== null && properties.kind == JSONValueKind.OBJECT) {
+//       const propsObject = properties.toObject();
 
-      const statusDescription = propsObject.get('statusDescription');
-      if (
-        statusDescription !== null &&
-        statusDescription.kind == JSONValueKind.STRING
-      ) {
-        entity.statusDescription = statusDescription.toString();
-      }
+//       const statusDescription = propsObject.get('statusDescription');
+//       if (
+//         statusDescription !== null &&
+//         statusDescription.kind == JSONValueKind.STRING
+//       ) {
+//         entity.statusDescription = statusDescription.toString();
+//       }
 
-      const taxonId = propsObject.get('taxonId');
-      if (taxonId !== null && taxonId.kind == JSONValueKind.STRING) {
-        entity.taxonId = taxonId.toString();
-      }
+//       const taxonId = propsObject.get('taxonId');
+//       if (taxonId !== null && taxonId.kind == JSONValueKind.STRING) {
+//         entity.taxonId = taxonId.toString();
+//       }
 
-      const conservationStatus = propsObject.get('conservationStatus');
-      if (
-        conservationStatus !== null &&
-        conservationStatus.kind == JSONValueKind.STRING
-      ) {
-        entity.conservationStatus = conservationStatus.toString();
-      }
+//       const conservationStatus = propsObject.get('conservationStatus');
+//       if (
+//         conservationStatus !== null &&
+//         conservationStatus.kind == JSONValueKind.STRING
+//       ) {
+//         entity.conservationStatus = conservationStatus.toString();
+//       }
 
-      const geometry = propsObject.get('geometry');
-      if (geometry !== null && geometry.kind == JSONValueKind.STRING) {
-        entity.geometry = geometry.toString();
-      }
-    }
-  }
+//       const geometry = propsObject.get('geometry');
+//       if (geometry !== null && geometry.kind == JSONValueKind.STRING) {
+//         entity.geometry = geometry.toString();
+//       }
+//     }
+//   }
 
-  return entity;
-}
+//   return entity;
+// }
---
+++ packages/constants/mocks/sampleCollection.ts
@@ -0,0 +1,7 @@
+export const IDENTIFIER_ADDRESS =
+  '1287:0xfd3434f66093f8007B30Bc44D3c467E660589059:0';
+
+export const BASE_URI = 'https://test-metadata/';
+
+export const COLLECTION_NAME = 'SNI-Sample';
+export const COLLECTION_SYMBOL = 'SNI-S';
