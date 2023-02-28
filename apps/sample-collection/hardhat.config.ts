4177ae3d0bf9cf477ce0ae35ad4ac6669172f159
Vadim Smakhtin <vadim@sovereignnature.com>
2023-02-28 10:32:40 +0100

    Deployment scripts for sample derrivative collection (#416)
    * Added deployment script
    
    * Mint script works

--- apps/sample-collection/hardhat.config.ts
+++ apps/sample-collection/hardhat.config.ts
@@ -1,6 +1,9 @@
 import '@nomicfoundation/hardhat-toolbox';
 import { moonbaseConfig } from '@sni/configs/hardhat/networks';
+import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
