353deb5d676caf70f94ae3ab06d17555f584609c
Vadim Smakhtin <vadim@sovereignnature.com>
2023-02-28 10:06:45 +0100

    Added deployment script

--- apps/sample-collection/scripts/deploy.ts
+++ apps/sample-collection/scripts/deploy.ts
@@ -1,20 +1,12 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const SampleContract = await ethers.getContractFactory('Sample');
+  const sample = await SampleContract.deploy();
 
-  const lockedAmount = ethers.utils.parseEther('1');
+  await sample.deployed();
 
-  const Lock = await ethers.getContractFactory('Lock');
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log('Deployed Sample collection to: ', sample.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
