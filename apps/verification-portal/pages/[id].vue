17d3d8269ebfff511e5300d6318b04b3d1a66200
Vadim Smakhtin <vadim@sovereignnature.com>
2023-07-31 16:08:08 +0200

    Added search for verification portal

--- apps/verification-portal/pages/[id].vue
+++ apps/verification-portal/pages/[id].vue
@@ -65,84 +65,64 @@ const people = [
 </script>
 
 <template>
-  <UCard>
-    <template #header>
-      <h1>Dynamic Page</h1>
-      <h1>{{ id }}</h1>
-    </template>
-    <div class="flex flex-row">
-      <div class="basis-1/3">
-        <UContainer
-          ><h1 class="text-xl">Linked Animal</h1>
-          <p>{{ data?.value?.createdAt }}</p>
+  <div class="flex flex-row">
+    <div class="basis-1/3">
+      <h1 class="text-xl text-white">Linked Animal {{ data?.id }}</h1>
+      <p>{{ data?.createdAt }}</p>
 
-          <div
-            class="inline-flex h-[30px] w-[111px] items-center justify-center gap-2 rounded-[5px] bg-emerald-200 px-[107px] py-[11px]">
-            <div class="text-center text-sm font-semibold text-black">
-              Support
-            </div>
-          </div>
-          <div class="relative h-1 w-[239px]">
-            <div
-              class="absolute left-0 top-0 h-1 w-[239px] rounded-[26px] bg-zinc-800"></div>
-            <div
-              class="absolute left-0 top-0 h-1 w-[97px] rounded-[26px] bg-gradient-to-l from-emerald-200 to-indigo-300"></div>
-          </div>
-          <img
-            class="h-10 w-10 rounded-full"
-            src="https://via.placeholder.com/27x29" />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <UTable :rows="people"
-        /></UContainer>
+      <div
+        class="inline-flex h-[30px] w-[111px] items-center justify-center gap-2 rounded-[5px] bg-emerald-200 px-[107px] py-[11px]">
+        <div class="text-center text-sm font-semibold text-black">Support</div>
       </div>
-      <div class="basis-1/3">
-        <UContainer
-          ><h1 class="text-xl">Funds Generated</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p></UContainer
-        >
+      <div class="relative h-1 w-[239px]">
+        <div
+          class="absolute left-0 top-0 h-1 w-[239px] rounded-[26px] bg-zinc-800"></div>
+        <div
+          class="absolute left-0 top-0 h-1 w-[97px] rounded-[26px] bg-gradient-to-l from-emerald-200 to-indigo-300"></div>
       </div>
-      <div class="basis-1/3">
-        <UContainer
-          ><h1 class="text-xl">Conservation Information</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p></UContainer
-        >
-      </div>
+      <img
+        class="h-10 w-10 rounded-full"
+        src="https://via.placeholder.com/27x29" />
+      <p class="text-white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <UTable :rows="people" />
     </div>
-
-    <!-- <UContainer>
-
-
-    </UContainer> -->
-    <template #footer />
-  </UCard>
+    <div class="basis-1/3">
+      <h1 class="text-xl">Funds Generated</h1>
+      <p class="text-white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </div>
+    <div class="basis-1/3">
+      <h1 class="text-xl">Conservation Information</h1>
+      <p class="text-white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </div>
+  </div>
 </template>
--- apps/verification-portal/pages/index.vue
+++ apps/verification-portal/pages/index.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-function submit(id: string) {
-  console.log(id);
-  navigateTo(`/${id}`);
-}
-</script>
-
 <template>
-  <UCard>
-    <template #header />
-    <UContainer>
-      <UInput
-        placeholder="Search..."
-        icon="i-heroicons-magnifying-glass-20-solid"
-        size="sm"
-        color="gray"
-        @keyup.enter="(e:InputEvent) => submit((e.target as HTMLInputElement).value)" />
-    </UContainer>
-    <template #footer />
-  </UCard>
+  <div class="flex h-screen w-full items-center">
+    <Search />
+  </div>
 </template>
