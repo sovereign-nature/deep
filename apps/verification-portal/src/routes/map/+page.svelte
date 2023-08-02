629b4242753a70ec52faefdf2d0a851b976a14ae
Vadim Smakhtin <vadim@sovereignnature.com>
2023-08-02 17:38:23 +0200

    Added simple map test

---
+++ apps/verification-portal/src/routes/map/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { browser } from '$app/environment';
+
+	let mapElement;
+	let map;
+
+	onMount(async () => {
+		if (browser) {
+			const leaflet = await import('leaflet');
+
+			map = leaflet.map(mapElement).setView([51.505, -0.09], 13);
+
+			leaflet
+				.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+					attribution:
+						'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				})
+				.addTo(map);
+
+			leaflet
+				.marker([51.5, -0.09])
+				.addTo(map)
+				.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+				.openPopup();
+		}
+	});
+
+	onDestroy(async () => {
+		if (map) {
+			console.log('Unloading Leaflet map.');
+			map.remove();
+		}
+	});
+</script>
+
+<div bind:this={mapElement} />
+
+<style>
+	@import 'leaflet/dist/leaflet.css';
+	div {
+		height: 800px;
+	}
+</style>
