108867aed81fd8b236ebb9791e85eb964fe08716
Vadim Smakhtin <vadim@sovereignnature.com>
2023-08-10 13:22:30 +0200

    Refactored event handlers

--- apps/indexer/src/deep-link.ts
+++ apps/indexer/src/deep-link.ts
@@ -3,134 +3,44 @@ import { log } from '@graphprotocol/graph-ts';
 import {
   Approval,
   ApprovalForAll,
-  ComputeURISet,
-  DataURISet,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  SovereignNatureIdentifier,
-  StatusSet,
-  TokenMinted,
-  TokenURISet,
+  DeepLink,
   Transfer,
-} from '../generated/SovereignNatureIdentifier/SovereignNatureIdentifier';
+} from '../generated/DeepLink/DeepLink';
 import { findEntity } from './utils';
 
-//TODO: Decide if we need approval events. Marketplace integration is not planned.
 export function handleApproval(event: Approval): void {
-  log.info('Approval event {}', [event.address.toHexString()]);
+  log.info('Approval event {}, not processed', [event.address.toHexString()]);
 }
 
 export function handleApprovalForAll(event: ApprovalForAll): void {
-  log.info('Approval for all event {}', [event.address.toHexString()]);
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChanged): void {
-  log.info('Role admin changed event {}', [event.address.toHexString()]);
-}
-
-export function handleRoleGranted(event: RoleGranted): void {
-  log.info('Role granted event {}', [event.address.toHexString()]);
-}
-
-export function handleRoleRevoked(event: RoleRevoked): void {
-  log.info('Role revoked event {}', [event.address.toHexString()]);
-}
-
-export function handleTokenMinted(event: TokenMinted): void {
-  log.info('Token minted event {}', [event.address.toHexString()]);
-}
-
-export function handleStatusSet(event: StatusSet): void {
-  const tokenId = event.params.tokenId;
-  const status = event.params.status;
-
-  const entity = findEntity(tokenId, event.block.timestamp);
-  entity.status = status;
-
-  entity.save();
-}
-
-export function handleTokenURISet(event: TokenURISet): void {
-  const tokenId = event.params.tokenId;
-  const tokenURI = event.params.tokenURI;
-  const tokenMetadataDigest = event.params.tokenURIDigest;
-
-  const entity = findEntity(tokenId, event.block.timestamp);
-  entity.tokenURI = tokenURI;
-  entity.tokenMetadataDigest = tokenMetadataDigest;
-
-  entity.save();
-}
-
-export function handleDataURISet(event: DataURISet): void {
-  const tokenId = event.params.tokenId;
-  const dataURI = event.params.dataURI;
-
-  const entity = findEntity(tokenId, event.block.timestamp);
-  entity.dataURI = dataURI;
-
-  entity.save();
-}
-
-export function handleComputeURISet(event: ComputeURISet): void {
-  const tokenId = event.params.tokenId;
-  const computeURI = event.params.computeURI;
-
-  const entity = findEntity(tokenId, event.block.timestamp);
-  entity.computeURI = computeURI;
-
-  entity.save();
+  log.info('Approval for all event {}, not processed', [
+    event.address.toHexString(),
+  ]);
 }
 
 export function handleTransfer(event: Transfer): void {
-  const contract = SovereignNatureIdentifier.bind(event.address);
+  const contract = DeepLink.bind(event.address);
 
   const tokenId = event.params.tokenId;
   const owner = event.params.to;
-  const status = contract.statusOf(tokenId);
 
   const tokenURI = contract.tokenURI(tokenId);
-  const tokenMetadataSchemaURI = contract.tokenURISchema(); //TODO: Consider refactoring methods naming in contract
-  const derivativeMetadataSchemaURI = contract.derivativeMetadataSchemaURI();
+  const elementId = contract.elementId(tokenId);
+  const conservationId = contract.conservationId(tokenId);
 
-  const tokenURIIntegrity = contract.tokenURIIntegrity(tokenId);
-  const tokenMetadataDigest = tokenURIIntegrity.value0;
-  const tokenMetadataHashFunction = tokenURIIntegrity.value1;
-
-  const tokenURISchemaIntegrity = contract.tokenURISchemaIntegrity();
-  const tokenMetadataSchemaDigest = tokenURISchemaIntegrity.value0;
-  const tokenMetadataSchemaHashFunction = tokenURISchemaIntegrity.value1;
-
-  const derivativeMetadataSchemaIntegrity =
-    contract.derivativeMetadataSchemaIntegrity();
-  const derivativeMetadataSchemaDigest =
-    derivativeMetadataSchemaIntegrity.value0;
-  const derivativeMetadataSchemaHashFunction =
-    derivativeMetadataSchemaIntegrity.value1;
-
-  const dataURI = contract.dataURI(tokenId);
-  const computeURI = contract.computeURI(tokenId);
+  const name = contract.name();
+  const symbol = contract.symbol();
 
   const entity = findEntity(tokenId, event.block.timestamp);
+
   entity.owner = owner;
-  entity.status = status;
-
   entity.tokenURI = tokenURI;
-  entity.tokenMetadataDigest = tokenMetadataDigest;
-  entity.tokenMetadataHashFunction = tokenMetadataHashFunction;
 
-  entity.tokenMetadataSchemaURI = tokenMetadataSchemaURI;
-  entity.tokenMetadataSchemaDigest = tokenMetadataSchemaDigest;
-  entity.tokenMetadataSchemaHashFunction = tokenMetadataSchemaHashFunction;
+  entity.elementId = elementId;
+  entity.conservationId = conservationId;
 
-  entity.derivativeMetadataSchemaURI = derivativeMetadataSchemaURI;
-  entity.derivativeMetadataSchemaDigest = derivativeMetadataSchemaDigest;
-  entity.derivativeMetadataSchemaHashFunction =
-    derivativeMetadataSchemaHashFunction;
-
-  entity.dataURI = dataURI;
-  entity.computeURI = computeURI;
+  entity.name = name;
+  entity.symbol = symbol;
 
   entity.save();
 }
--- apps/indexer/src/utils.ts
+++ apps/indexer/src/utils.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { BigInt } from '@graphprotocol/graph-ts';
-import { SNI } from '../generated/schema';
+import { DeepLink } from '../generated/schema';
 
 // Find entity in database or create a new one if it's not found.
 // eslint-disable-next-line @typescript-eslint/ban-types
-export function findEntity(tokenId: BigInt, blockTimestamp: BigInt): SNI {
+export function findEntity(tokenId: BigInt, blockTimestamp: BigInt): DeepLink {
   const id = tokenId.toHex();
-  let entity = SNI.load(id);
+  let entity = DeepLink.load(id);
 
   if (!entity) {
-    entity = new SNI(id);
+    entity = new DeepLink(id);
     entity.tokenId = tokenId;
     entity.createdAt = blockTimestamp;
   } else {
@@ -18,61 +18,3 @@ export function findEntity(tokenId: BigInt, blockTimestamp: BigInt): SNI {
 
   return entity;
 }
-
-// Fill entity data from IPFS metadata file.
-// export function fillFromIPFS(entity: SNI, tokenURI: string): SNI {
-//   const data = ipfs.cat(tokenURI.replace('ipfs://', ''));
-//   log.info('Reindexing: {}', ['4']);
-
-//   if (data !== null) {
-//     const metadata = json.fromBytes(data).toObject();
-
-//     const name = metadata.get('name');
-//     if (name !== null && name.kind == JSONValueKind.STRING) {
-//       entity.name = name.toString();
-//     }
-
-//     const description = metadata.get('description');
-//     if (description !== null && description.kind == JSONValueKind.STRING) {
-//       entity.description = description.toString();
-//     }
-
-//     const image = metadata.get('image');
-//     if (image !== null && image.kind == JSONValueKind.STRING) {
-//       entity.image = image.toString();
-//     }
-
-//     const properties = metadata.get('properties');
-//     if (properties !== null && properties.kind == JSONValueKind.OBJECT) {
-//       const propsObject = properties.toObject();
-
-//       const statusDescription = propsObject.get('statusDescription');
-//       if (
-//         statusDescription !== null &&
-//         statusDescription.kind == JSONValueKind.STRING
-//       ) {
-//         entity.statusDescription = statusDescription.toString();
-//       }
-
-//       const taxonId = propsObject.get('taxonId');
-//       if (taxonId !== null && taxonId.kind == JSONValueKind.STRING) {
-//         entity.taxonId = taxonId.toString();
-//       }
-
-//       const conservationStatus = propsObject.get('conservationStatus');
-//       if (
-//         conservationStatus !== null &&
-//         conservationStatus.kind == JSONValueKind.STRING
-//       ) {
-//         entity.conservationStatus = conservationStatus.toString();
-//       }
-
-//       const geometry = propsObject.get('geometry');
-//       if (geometry !== null && geometry.kind == JSONValueKind.STRING) {
-//         entity.geometry = geometry.toString();
-//       }
-//     }
-//   }
-
-//   return entity;
-// }
