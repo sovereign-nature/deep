69cc19237bec61a8e80de4e3636751ba3d319044
Vadim Smakhtin <vadim@sovereignnature.com>
2023-08-11 23:26:57 +0200

    Eslint and prettier configs works

--- apps/verification-portal/src/routes/+page.server.ts
+++ apps/verification-portal/src/routes/+page.server.ts
@@ -1,9 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 
 export const actions = {
-	default: async (event) => {
-		const data = await event.request.formData();
+  default: async (event) => {
+    const data = await event.request.formData();
 
-		throw redirect(307, `/${data.get('search')}`);
-	}
+    throw redirect(307, `/${data.get('search')}`);
+  },
 };
