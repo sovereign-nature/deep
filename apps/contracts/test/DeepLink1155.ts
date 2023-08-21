4ecedb4c7bc17d387f9d628ad609987c1a53ff2a
Vadim Smakhtin <vadim@sovereignnature.com>
2023-08-21 17:02:25 +0200

    Added tests for 1155

--- apps/contracts/test/DeepLink1155.ts
+++ apps/contracts/test/DeepLink1155.ts
@@ -1,91 +1,85 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import { deployDeepLink1155Fixture } from './fixtures';
+import {
+  deployDeepLink1155BatchFixture,
+  deployDeepLink1155SingleFixture,
+} from './fixtures';
 
-describe('DeepLink1155 Single Token Minting', function () {
-  it('Should mint', async function () {
-    const { mintInitial } = await loadFixture(deployDeepLink1155Fixture);
+import { CONSERVATION_IDS, ELEMENT_IDS } from '@sni/constants/mocks/deep-link';
 
-    expect(await mintInitial()).not.to.be.reverted;
-  });
+const tests = [
+  { mintingType: 'single', fixture: deployDeepLink1155SingleFixture },
+  { mintingType: 'batch', fixture: deployDeepLink1155BatchFixture },
+];
 
-  it('Should has initial owner', async function () {
-    const { deepLink, owner, mintInitial } = await loadFixture(
-      deployDeepLink1155Fixture
-    );
+describe('DeepLink1155 Token Minting', function () {
+  tests.forEach(({ mintingType, fixture }) => {
+    it(`Should mint ${mintingType}`, async function () {
+      const { mintInitial } = await loadFixture(fixture);
 
-    await mintInitial();
+      expect(await mintInitial()).not.to.be.reverted;
+    });
 
-    const nftOwner = await deepLink.owner();
+    it(`Should has initial owner when minting ${mintingType}`, async function () {
+      const { deepLink, owner, mintInitial } = await loadFixture(fixture);
 
-    expect(nftOwner).to.equal(owner.address);
-  });
+      await mintInitial();
 
-  it('Should has element ID set', async function () {
-    const { deepLink, initialTokenId, mintInitial, initialElementId } =
-      await loadFixture(deployDeepLink1155Fixture);
+      const nftOwner = await deepLink.owner();
 
-    await mintInitial();
+      expect(nftOwner).to.equal(owner.address);
+    });
 
-    const elementId = await deepLink.elementId(initialTokenId);
+    it(`Should has element ID set when minting ${mintingType}`, async function () {
+      const { deepLink, initialTokenId, mintInitial, initialElementId } =
+        await loadFixture(fixture);
 
-    expect(elementId).to.equal(initialElementId);
-  });
+      await mintInitial();
 
-  it('Should has conservation ID set', async function () {
-    const { deepLink, initialTokenId, mintInitial, initialConservationId } =
-      await loadFixture(deployDeepLink1155Fixture);
+      const elementId = await deepLink.elementId(initialTokenId);
 
-    await mintInitial();
+      expect(elementId).to.equal(initialElementId);
+    });
 
-    const conservationId = await deepLink.conservationId(initialTokenId);
+    it(`Should has conservation ID set when minting ${mintingType}`, async function () {
+      const { deepLink, initialTokenId, mintInitial, initialConservationId } =
+        await loadFixture(fixture);
 
-    expect(conservationId).to.equal(initialConservationId);
+      await mintInitial();
+
+      const conservationId = await deepLink.conservationId(initialTokenId);
+
+      expect(conservationId).to.equal(initialConservationId);
+    });
   });
 });
 
-describe('DeepLink1155 Batch Token Minting', function () {
-  it('Should mint', async function () {
-    const { mintInitialBatch } = await loadFixture(deployDeepLink1155Fixture);
-
-    expect(await mintInitialBatch()).not.to.be.reverted;
-  });
-
-  it('Should has initial owner', async function () {
-    const { deepLink, owner, mintInitialBatch } = await loadFixture(
-      deployDeepLink1155Fixture
+describe('DeepLink1155 Batch Data Verification', function () {
+  it(`Should has all element IDs set when minting batch`, async function () {
+    const { deepLink, mintInitial } = await loadFixture(
+      deployDeepLink1155BatchFixture
     );
 
-    await mintInitialBatch();
+    await mintInitial();
 
-    const nftOwner = await deepLink.owner();
+    for (let i = 0; i < ELEMENT_IDS.length; i++) {
+      const elementId = await deepLink.elementId(i);
 
-    expect(nftOwner).to.equal(owner.address);
+      expect(elementId).to.equal(ELEMENT_IDS[i]);
+    }
   });
 
-  it('Should has element ID set', async function () {
-    const { deepLink, initialTokenId, mintInitialBatch, initialElementId } =
-      await loadFixture(deployDeepLink1155Fixture);
+  it(`Should has all conservation IDs set when minting batch`, async function () {
+    const { deepLink, mintInitial } = await loadFixture(
+      deployDeepLink1155BatchFixture
+    );
 
-    await mintInitialBatch();
+    await mintInitial();
 
-    const elementId = await deepLink.elementId(initialTokenId);
+    for (let i = 0; i < CONSERVATION_IDS.length; i++) {
+      const conservationId = await deepLink.conservationId(i);
 
-    expect(elementId).to.equal(initialElementId);
-  });
-
-  it('Should has conservation ID set', async function () {
-    const {
-      deepLink,
-      initialTokenId,
-      mintInitialBatch,
-      initialConservationId,
-    } = await loadFixture(deployDeepLink1155Fixture);
-
-    await mintInitialBatch();
-
-    const conservationId = await deepLink.conservationId(initialTokenId);
-
-    expect(conservationId).to.equal(initialConservationId);
+      expect(conservationId).to.equal(CONSERVATION_IDS[i]);
+    }
   });
 });
--- packages/constants/mocks/deep-link.ts
+++ packages/constants/mocks/deep-link.ts
@@ -1,6 +1,7 @@
 export const TOKEN_URI = 'ipfs://token-uri/metadata.json';
 
 export const INITIAL_TOKEN_ID = 0;
+export const INITIAL_TOKEN_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 export const TEMP_TOKEN_ID = 99;
 
 export const MINTER_ADDRESS = '0x0000000000000000000000000000000000000000';
@@ -8,8 +9,32 @@ export const OWNER_ADDRESS = '0x0000000000000000000000000000000000000001';
 export const SECOND_OWNER_ADDRESS =
   '0x0000000000000000000000000000000000000002';
 
-export const ELEMENT_ID = 'lion-128';
+export const ELEMENT_ID = 'lion-1';
+export const ELEMENT_IDS = [
+  'lion-1',
+  'lion-2',
+  'lion-3',
+  'lion-4',
+  'lion-5',
+  'lion-6',
+  'lion-7',
+  'lion-8',
+  'lion-9',
+  'lion-10',
+];
 export const CONSERVATION_ID = 'kwt';
+export const CONSERVATION_IDS = [
+  'kwt',
+  'kwt',
+  'kwt',
+  'kwt',
+  'kwt',
+  'kwt',
+  'kwt',
+  'kwt',
+  'kwt',
+  'kwt',
+];
 
 export const NAME = 'DeepLink';
 export const SYMBOL = 'DLK';
