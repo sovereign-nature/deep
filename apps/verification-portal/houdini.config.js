7e98fe7207f188327a4f4714afdb21bace03c6ff
Vadim Smakhtin <vadim@sovereignnature.com>
2023-09-15 15:28:21 +0100

    GQL starts

--- apps/verification-portal/houdini.config.js
+++ apps/verification-portal/houdini.config.js
@@ -1,5 +1,10 @@
+/// <references types="houdini-svelte">
+
 /** @type {import('houdini').ConfigFile} */
 const config = {
+  watchSchema: {
+    url: 'https://squid.subsquid.io/speck/v/v3/graphql',
+  },
   plugins: {
     'houdini-svelte': {},
   },
