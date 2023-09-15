ddcffe53c01491a6a1290f3d70571fb9d7317022
Vadim Smakhtin <vadim@sovereignnature.com>
2023-09-15 13:53:24 +0100

    Separated address building and id generation

--- packages/address-utils/index.ts
+++ packages/address-utils/index.ts
@@ -6,18 +6,30 @@ export function stringToId(input: string): string {
   return BigInt(hash).toString();
 }
 
+//standard:vendorId.assetType:assetID
+//deep:hh.asset:african-elephant-mask
+export function getOffChainAssetAddress(
+  vendorId: string,
+  assetName: string
+): string {
+  return `deep:${vendorId}.asset:${assetName.toLowerCase().replace(' ', '-')}`;
+}
+
 export function getOffChainAssetId(
   vendorId: string,
   assetName: string
 ): string {
-  //standard:vendorId.assetType:assetID
-  //deep:hh.asset:african-elephant-mask
+  return stringToId(getOffChainAssetAddress(vendorId, assetName));
+}
 
-  const assetAddress = `deep:${vendorId}.asset:${assetName
-    .toLowerCase()
-    .replace(' ', '-')}`;
-
-  return stringToId(assetAddress);
+//'eip155:1.erc721:0x06012c8cf97bead5deae237070f9587f8e7a266d:634446'
+export function getEVMAssetAddress(
+  chainId: number,
+  tokenStandard: string,
+  contractAddress: string,
+  tokenId: number
+) {
+  return `eip155:${chainId}.${tokenStandard}:${contractAddress}:${tokenId}`;
 }
 
 export function getEVMAssetId(
@@ -26,21 +38,23 @@ export function getEVMAssetId(
   contractAddress: string,
   tokenId: number
 ): string {
-  //'eip155:1.erc721:0x06012c8cf97bead5deae237070f9587f8e7a266d:634446'
-
-  const assetAddress = `eip155:${chainId}.${tokenStandard}:${contractAddress}:${tokenId}`;
-
-  return stringToId(assetAddress);
+  return stringToId(
+    getEVMAssetAddress(chainId, tokenStandard, contractAddress, tokenId)
+  );
 }
 
+//deep:kusama.asset-hub:91:10
+export function getPolkadotAssetHubAddress(
+  chainId: string,
+  collectionId: string,
+  tokenId: number
+) {
+  return `deep:${chainId}.asset-hub:${collectionId}:${tokenId}`;
+}
 export function getPolkadotAssetId(
   chainId: string,
   collectionId: string,
   tokenId: number
 ): string {
-  //deep:kusama.asset-hub:91:10
-
-  const assetAddress = `deep:${chainId}.asset-hub:${collectionId}:${tokenId}`;
-
-  return stringToId(assetAddress);
+  return stringToId(getPolkadotAssetHubAddress(chainId, collectionId, tokenId));
 }
