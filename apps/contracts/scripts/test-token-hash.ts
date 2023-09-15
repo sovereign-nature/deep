2a6aa6ff1772d877dba53e034536d149826b5a52
Vadim Smakhtin <vadim@sovereignnature.com>
2023-09-15 11:07:37 +0100

    Upgraded token hash script to ethers v6

--- apps/contracts/scripts/test-token-hash.ts
+++ apps/contracts/scripts/test-token-hash.ts
@@ -5,10 +5,9 @@ const assetAddress =
   'eip155:1.erc721:0x06012c8cf97bead5deae237070f9587f8e7a266d:634446';
 // const normalized = ethers.utils.nameprep(input);
 // const hashed = ethers.utils.namehash(normalized);
-const labelHash = ethers.utils.keccak256(
-  ethers.utils.toUtf8Bytes(assetAddress)
-);
-const tokenId = ethers.BigNumber.from(labelHash).toString();
+const labelHash = ethers.keccak256(ethers.toUtf8Bytes(assetAddress));
+
+const tokenId = BigInt(labelHash).toString();
 
 console.log(`Input: ${assetAddress}`);
 console.log(`Label Hash: ${labelHash}`);
