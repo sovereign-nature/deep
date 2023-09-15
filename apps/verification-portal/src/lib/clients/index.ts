9e679eaa6852c31e725b71989944f5499ee3f473
Vadim Smakhtin <vadim@sovereignnature.com>
2023-09-15 15:37:50 +0100

    Removed houdini

--- apps/verification-portal/src/lib/clients/index.ts
+++
@@ -1,9 +0,0 @@
-import { HoudiniClient } from '$houdini';
-
-export const polkadotGqlClient = new HoudiniClient({
-  url: 'https://squid.subsquid.io/speck/v/v3/graphql',
-});
-
-export const kusamaGqlClient = new HoudiniClient({
-  url: 'https://squid.subsquid.io/stick/graphql',
-});
--- apps/verification-portal/vite.config.ts
+++ apps/verification-portal/vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import houdini from 'houdini/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [houdini(), sveltekit()],
+  plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
     watch: false,
