8468b490b07c40e843baf4f881b4d3dc70334da3
Vadim Smakhtin <vadim@sovereignnature.com>
2023-09-22 11:20:53 +0100

    Crossorigin fonts accessed with anonymous

--- apps/verification-portal/src/routes/+layout.svelte
+++ apps/verification-portal/src/routes/+layout.svelte
@@ -4,7 +4,11 @@
 
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin="anonymous"
+  />
   <link
     href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
     rel="stylesheet"
