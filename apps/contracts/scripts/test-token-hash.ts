eee673aa1f272d7b8c7d17004d8c539200438d39
Vadim Smakhtin <vadim@sovereignnature.com>
2023-09-22 10:20:19 +0100

    Different hash test case in script

--- apps/contracts/scripts/test-token-hash.ts
+++ apps/contracts/scripts/test-token-hash.ts
@@ -4,12 +4,11 @@
 import { ethers } from 'hardhat';
 
 //Asset DID spec https://github.com/KILTprotocol/spec-asset-did
-const assetAddress =
-  'eip155:1.erc721:0x06012c8cf97bead5deae237070f9587f8e7a266d:634446';
+const assetAddress = 'did:asset:deep:polkadot.asset-hub:u-8:262';
 // const normalized = ethers.utils.nameprep(input);
 // const hashed = ethers.utils.namehash(normalized);
-const labelHash = ethers.keccak256(ethers.toUtf8Bytes(assetAddress));
 
+const labelHash = ethers.keccak256(ethers.toUtf8Bytes(assetAddress));
 const tokenId = BigInt(labelHash).toString();
 
 console.log(`Input: ${assetAddress}`);
