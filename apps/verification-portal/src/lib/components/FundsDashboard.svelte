b17e31067b03b18e8e91ba773c6cef1e24619f93
Maija <hello@maijagrudule.com>
2023-09-25 17:18:33 +0100

    Minor fixes &

--- apps/verification-portal/src/lib/components/FundsDashboard.svelte
+++ apps/verification-portal/src/lib/components/FundsDashboard.svelte
@@ -6,10 +6,10 @@
     <span class="text-sm xl:text-black">By this item</span>
   </div>
 
-  <div class="text-center">
+  <!-- <div class="text-center">
     <span class="bg-white h-24 w-24 rounded-full block m-4"></span>
     <span class="text-sm xl:text-black">By all copies</span>
-  </div>
+  </div> -->
   <div class="text-center">
     <span class="bg-deep-green h-36 w-36 rounded-full block m-4"></span>
     <span class="text-sm text-primary-300 xl:text-black"
