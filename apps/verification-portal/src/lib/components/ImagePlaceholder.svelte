28f13fe7ddcae9c4cc7eada81b480be199e4459d
Maija <hello@maijagrudule.com>
2023-09-28 00:26:23 +0100

    Placeholder load card components

---
+++ apps/verification-portal/src/lib/components/ImagePlaceholder.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let className: string = '';
+  export let colorStart = 'rgba(255, 255, 255, 0.1)';
+  export let colorEnd = 'rgba(255, 255, 255, 0)';
+</script>
+
+<div
+  class="{className} placeholder"
+  style="--gradient-color-start: {colorStart}; --gradient-color-end: {colorEnd};"
+>
+  <slot />
+</div>
+
+<style>
+  .placeholder {
+    position: relative;
+    width: 100%;
+    min-height: inherit;
+    display: block;
+    background: linear-gradient(
+      to right,
+      var(--gradient-color-start),
+      var(--gradient-color-end),
+      var(--gradient-color-start)
+    );
+    background-size: 200% 100%;
+    animation: placeholderAnimation 3s linear infinite;
+  }
+  @keyframes placeholderAnimation {
+    0% {
+      background-position: 200% 0;
+    }
+    100% {
+      background-position: -200% 0;
+    }
+  }
+</style>
