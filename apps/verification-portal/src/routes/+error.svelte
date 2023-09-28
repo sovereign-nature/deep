a8a48d06aa6a9ba7e3b9e3de4461fb6dca4b400b
Maija <hello@maijagrudule.com>
2023-09-28 17:06:06 +0100

    Layout component

--- apps/verification-portal/src/routes/+error.svelte
+++ apps/verification-portal/src/routes/+error.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import NavBar from '$lib/components/NavBar.svelte';
   import Search from '$lib/components/Search.svelte';
+  import LayoutLanding from '$lib/containers/LayoutLanding.svelte';
 </script>
 
-<div class="dark w-full flex flex-col min-h-screen content-center bg-Sni-img">
-  <NavBar themeSwitch={false}></NavBar>
-  <div class="container px-5 min-h-screen">
-    <div class="w-full lg:w-4/5 mx-auto pt-12 md:pt-40">
-      <h1
-        class="text-3xl text-white mb-8 md:text-left text-center sm:text-4xl lg:text-5xl"
-      >
-        <span class="text-primary-300 block md:inline"> {$page.status}</span>
-        <span class="text-primary-300 hidden md:inline">| </span>
-        {$page.error?.message}
-      </h1>
-      <h2
-        class="text-base sm:text-2xl lg:text-3xl font-serif text-white md:text-left text-center mb-8"
-      >
-        Search for your asset to verify
-      </h2>
-      <Search />
-    </div>
-  </div>
-</div>
+<LayoutLanding>
+  <h1
+    class="text-3xl text-white mb-8 md:text-left text-center sm:text-4xl lg:text-5xl"
+  >
+    <span class="text-primary-300 block md:inline"> {$page.status}</span>
+    <span class="text-primary-300 hidden md:inline">| </span>
+    {$page.error?.message}
+  </h1>
+  <h2
+    class="text-base sm:text-2xl lg:text-3xl text-white md:text-left text-center mb-8"
+  >
+    Search for your asset to verify
+  </h2>
+  <Search />
+</LayoutLanding>
--- apps/verification-portal/src/routes/+page.svelte
+++ apps/verification-portal/src/routes/+page.svelte
@@ -1,21 +1,16 @@
 <script>
-  import NavBar from '$lib/components/NavBar.svelte';
   import Search from '$lib/components/Search.svelte';
+  import LayoutLanding from '$lib/containers/LayoutLanding.svelte';
   import ShareCard from '$lib/components/ShareCard.svelte';
 </script>
 
 <ShareCard />
 
-<div class="dark w-full flex flex-col min-h-screen content-center bg-Sni-img">
-  <NavBar themeSwitch={false}></NavBar>
-  <div class="container px-5">
-    <div class="w-full lg:w-4/5 mx-auto pt-12 md:pt-64">
-      <h1
-        class="text-3xl text-white mb-8 md:text-left text-center sm:text-4xl lg:text-5xl"
-      >
-        Search for your asset to verify
-      </h1>
-      <Search />
-    </div>
-  </div>
-</div>
+<LayoutLanding>
+  <h1
+    class=" text-white mb-8 md:text-left text-center text-3xl sm:text-4xl lg:text-5xl"
+  >
+    Search for your asset to verify
+  </h1>
+  <Search />
+</LayoutLanding>
