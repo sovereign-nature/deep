71925749a6e0cc6bcc2d0acfe4111bf9ae077d15
Vadim Smakhtin <vadim@sovereignnature.com>
2023-10-05 13:30:11 +0200

    Initial sentry integration

--- apps/verification-portal/vite.config.ts
+++ apps/verification-portal/vite.config.ts
@@ -1,8 +1,17 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'sovereign-nature-initiative',
+        project: 'real',
+      },
+    }),
+    sveltekit(),
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
     watch: false,
