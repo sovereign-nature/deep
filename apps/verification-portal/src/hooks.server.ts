797b9c6f25dff8f0d10685d7b1108dd72ab07545
Vadim Smakhtin <vadim@sovereignnature.com>
2023-10-13 15:34:11 +0200

    Added environment check for sentry

--- apps/verification-portal/src/hooks.server.ts
+++ apps/verification-portal/src/hooks.server.ts
@@ -1,11 +1,14 @@
+import { dev } from '$app/environment';
 import * as Sentry from '@sentry/sveltekit';
 import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-Sentry.init({
-  dsn: 'https://edc41182fe9540beb0e8a11b58b7172b@o4505997445824512.ingest.sentry.io/4505997447135232',
-  tracesSampleRate: 1.0,
-});
+if (!dev) {
+  Sentry.init({
+    dsn: 'https://edc41182fe9540beb0e8a11b58b7172b@o4505997445824512.ingest.sentry.io/4505997447135232',
+    tracesSampleRate: 1.0,
+  });
+}
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
 export const handle = sequence(sentryHandle());
