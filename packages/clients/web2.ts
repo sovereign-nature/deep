31086cdcdbe28fa7b012ab0f69370f7af2e4765a
Vadim Smakhtin <vadim@sovereignnature.com>
2023-10-16 15:32:24 +0200

    Updated tests for web2 collections

--- packages/clients/web2.ts
+++ packages/clients/web2.ts
@@ -1,7 +1,9 @@
 import { SNI_API_URL } from '@sni/constants';
 
 export function getHotelHideawayAsset(id: string) {
-  return fetch(`${SNI_API_URL}/items/hotel_hideaway/${id}`);
+  return fetch(
+    `${SNI_API_URL}/items/hotel_hideaway/${id}?fields=*,collection.*`
+  );
 }
 
 export function getWeb2Asset(gameId: string, assetId: string) {
