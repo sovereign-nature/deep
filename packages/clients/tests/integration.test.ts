30f4c03a656b2d0f04fe2ca543533bb934384882
Vadim Smakhtin <vadim@sovereignnature.com>
2023-10-16 15:06:33 +0200

    Simplified integration tests, removed axios from web2

--- packages/clients/tests/integration.test.ts
+++ packages/clients/tests/integration.test.ts
@@ -4,59 +4,55 @@ import { getLinkByAddress, getLinkById } from '../link';
 import { getNftData } from '../nft';
 import { getWeb2Asset } from '../web2';
 import {
-  entityResponse,
   hotelHideawayResponse,
   kusamaNftResponse,
-  linkResponse,
   polkadotNftResponse,
-  stewardResponse,
 } from './responses';
 
 test('getLinkById', async () => {
   const response = await getLinkById(
-    '31537534386950388023491254677150515860389712061535283602024150019331823461691'
+    '100159747025278469334864584271547111665257165513221447159618761990373607040690'
   );
 
-  expect(response.data).toStrictEqual(linkResponse);
+  expect(response.status).toBe(200);
 });
 
 test('getLinkByAddress', async () => {
   const response = await getLinkByAddress(
-    'did:asset:deep:polkadot.asset-hub:u-8:262'
+    'did:asset:deep:polkadot.asset-hub:13:10'
   );
 
-  expect(response.data).toStrictEqual(linkResponse);
+  expect(response.status).toBe(200);
 });
 
 test('getEntity', async () => {
   const response = await getEntity('aimm_minke_whales', 'ba_alg_001');
 
-  expect(response.data).toStrictEqual(entityResponse);
+  expect(response.status).toBe(200);
 });
 
 test('getSteward', async () => {
   const response = await getSteward('aimm');
 
-  expect(response.data).toStrictEqual(stewardResponse);
+  expect(response.status).toBe(200);
 });
 
 test('getNFTData from Polkadot', async () => {
-  const response = await getNftData('polkadot', 'u-8', 262);
+  const response = (await getNftData('polkadot', 'u-8', 262)) as object;
 
   expect(response).toStrictEqual(polkadotNftResponse);
 });
 
 test('getNFTData from Kusama', async () => {
-  const response = await getNftData('kusama', '91', 10);
+  const response = (await getNftData('kusama', '91', 10)) as object;
 
   expect(response).toStrictEqual(kusamaNftResponse);
 });
 
 test('getWeb2Asset from Hotel Hideaway', async () => {
-  const response = await getWeb2Asset(
-    'hotel-hideaway',
-    'congolese-elephant-headwrap'
-  );
+  const data = await (
+    await getWeb2Asset('hotel-hideaway', 'congolese-elephant-headwrap')
+  ).json();
 
-  expect(response.data).toStrictEqual(hotelHideawayResponse);
+  expect(data).toStrictEqual(hotelHideawayResponse);
 });
--- packages/clients/web2.ts
+++ packages/clients/web2.ts
@@ -1,27 +1,13 @@
 import { SNI_API_URL } from '@sni/constants';
-import type { AxiosRequestConfig } from 'axios';
 
 export function getHotelHideawayAsset(id: string) {
-  console.log(id);
   return fetch(`${SNI_API_URL}/items/hotel_hideaway/${id}`);
 }
 
-function getFortniteAsset(assetId: string, config: AxiosRequestConfig = {}) {
-  console.log('getFortniteAsset', assetId);
-  console.log('config', config);
-  throw new Error('Not implemented');
-}
-
-export function getWeb2Asset(
-  gameId: string,
-  assetId: string,
-  config: AxiosRequestConfig = {}
-) {
+export function getWeb2Asset(gameId: string, assetId: string) {
   switch (gameId) {
     case 'hotel-hideaway':
       return getHotelHideawayAsset(assetId);
-    case 'fortnite':
-      return getFortniteAsset(assetId, config);
     default:
       throw new Error(`Unsupported game ID: ${gameId}`);
   }
