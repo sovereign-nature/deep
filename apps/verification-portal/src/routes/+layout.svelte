ca27816c61ecdfd0a79f613ee6f293955d508e89
Maija <hello@maijagrudule.com>
2023-10-21 23:13:04 +0100

    REA-10 added page load indicator

--- apps/verification-portal/src/routes/+layout.svelte
+++ apps/verification-portal/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
   import '../app.postcss';
+  import { fade } from 'svelte/transition';
+  import { beforeNavigate, afterNavigate } from '$app/navigation';
+
+  let isLoading = false;
+
+  beforeNavigate(() => (isLoading = true));
+  afterNavigate(() => (isLoading = false));
+
+  export let data;
 </script>
 
 <svelte:head>
@@ -14,5 +23,31 @@
     rel="stylesheet"
   />
 </svelte:head>
+{#if isLoading}
+  <!-- <div class="absolute z-60 top-0 left-0 w-full h-1 bg-primary-300 block"></div> -->
+  <div
+    class="fixed top-0 left-0 h-1 bg-primary-300 w-0 animate-loading-bar"
+  ></div>
+{/if}
 
-<slot />
+{#key data.pathname}
+  <div in:fade={{ duration: 200, delay: 100 }} out:fade={{ duration: 100 }}>
+    <slot />
+  </div>
+{/key}
+
+<style>
+  @keyframes loading-bar-animation {
+    0% {
+      width: 0%;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+
+  .animate-loading-bar {
+    animation: loading-bar-animation 2s ease-out infinite;
+    animation-delay: 0.2s;
+  }
+</style>
