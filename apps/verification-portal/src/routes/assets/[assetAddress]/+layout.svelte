738e73629b1b0030372fa102b0164a092078994a
Vadim Smakhtin <vadim@sovereignnature.com>
2024-01-03 14:13:35 +0100

    Commented out search button REA-66 (#803)
    * Commented out search button
    
    * Import cleanup

--- apps/verification-portal/src/routes/assets/[assetAddress]/+layout.svelte
+++ apps/verification-portal/src/routes/assets/[assetAddress]/+layout.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
   import NavBar from '$lib/components/navbar/index.svelte';
-  import SearchIcon from '$lib/components/icons/SearchIcon.svelte';
-  // import SearchSm from '$lib/components/search/SearchSM.svelte';
   import Footer from '$lib/components/Footer.svelte';
-  import RolloverBtn from '$lib/components/RolloverBtn.svelte';
 </script>
 
 <div class="w-full flex flex-col min-h-screen content-center justify-center">
   <NavBar>
-    <RolloverBtn type="secondary" url="/" ariaLabel="search">
+    <!-- <RolloverBtn type="secondary" url="/" ariaLabel="search">
       <SearchIcon slot="icon" className="h-4 w-4 mx-1 "></SearchIcon>
-    </RolloverBtn>
+    </RolloverBtn> -->
   </NavBar>
   <div class="w-full m-auto">
     <div class="container mx-auto py-8">
