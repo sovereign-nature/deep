5d73f90f31797a7a15796102a5954c9c64681c58
Maija <hello@maijagrudule.com>
2024-01-09 23:37:32 +0200

    Disabled features

--- apps/verification-portal/src/hooks.client.ts
+++ apps/verification-portal/src/hooks.client.ts
@@ -1,24 +1,20 @@
-import { dev } from '$app/environment';
-import * as Sentry from '@sentry/sveltekit';
-import { handleErrorWithSentry, Replay } from '@sentry/sveltekit';
+// if (!dev) {
+//   Sentry.init({
+//     dsn: 'https://edc41182fe9540beb0e8a11b58b7172b@o4505997445824512.ingest.sentry.io/4505997447135232',
+//     tracesSampleRate: 1.0,
 
-if (!dev) {
-  Sentry.init({
-    dsn: 'https://edc41182fe9540beb0e8a11b58b7172b@o4505997445824512.ingest.sentry.io/4505997447135232',
-    tracesSampleRate: 1.0,
+//     // This sets the sample rate to be 10%. You may want this to be 100% while
+//     // in development and sample at a lower rate in production
+//     replaysSessionSampleRate: 0.1,
 
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
+//     // If the entire session is not sampled, use the below sample rate to sample
+//     // sessions when an error occurs.
+//     replaysOnErrorSampleRate: 1.0,
 
-    // If the entire session is not sampled, use the below sample rate to sample
-    // sessions when an error occurs.
-    replaysOnErrorSampleRate: 1.0,
-
-    // If you don't want to use Session Replay, just remove the line below:
-    integrations: [new Replay()],
-  });
-}
+//     // If you don't want to use Session Replay, just remove the line below:
+//     integrations: [new Replay()],
+//   });
+// }
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+// export const handleError = handleErrorWithSentry();
--- apps/verification-portal/src/lib/config/siteConfigs.ts
+++ apps/verification-portal/src/lib/config/siteConfigs.ts
@@ -3,9 +3,9 @@ export default {
   siteDescription:
     'The eco-data pipeline to connect real life ecology with the digital world.',
   feature: {
-    walletEnabled: true,
-    POCTabEnabled: true,
-    notificationsEnabled: true,
+    walletEnabled: false,
+    POCTabEnabled: false,
+    notificationsEnabled: false,
     testGMInbox: false,
   },
 };
