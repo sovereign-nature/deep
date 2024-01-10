004ebb1f7d741635685c767647bdebac6f2f2808
Vadim Smakhtin <vadim@sovereignnature.com>
2024-01-10 12:55:36 +0000

    Returned Sentry hooks

--- apps/verification-portal/src/hooks.server.ts
+++ apps/verification-portal/src/hooks.server.ts
@@ -1,12 +1,17 @@
-// if (!dev) {
-//   Sentry.init({
-//     dsn: 'https://edc41182fe9540beb0e8a11b58b7172b@o4505997445824512.ingest.sentry.io/4505997447135232',
-//     tracesSampleRate: 1.0,
-//   });
-// }
+import { dev } from '$app/environment';
+import * as Sentry from '@sentry/sveltekit';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-// // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-// export const handle = sequence(sentryHandle());
+if (!dev) {
+  Sentry.init({
+    dsn: 'https://edc41182fe9540beb0e8a11b58b7172b@o4505997445824512.ingest.sentry.io/4505997447135232',
+    tracesSampleRate: 1.0,
+  });
+}
 
-// // If you have a custom error handler, pass it to `handleErrorWithSentry`
-// export const handleError = handleErrorWithSentry();
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
