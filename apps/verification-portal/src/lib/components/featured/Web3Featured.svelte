c59161168faa89acda7cf08c928985d2a05a1df9
Vadim Smakhtin <vadim@sovereignnature.com>
2024-01-15 16:32:21 +0100

    highlights are still crashing

--- apps/verification-portal/src/lib/components/featured/Web3Featured.svelte
+++ apps/verification-portal/src/lib/components/featured/Web3Featured.svelte
@@ -3,9 +3,12 @@
   import Web3Featured from '$lib/data/Web3Featured.json'; //TODO: Replace with API call to highlights services
   import type { AssetFeatured } from '$lib/types';
   import { shuffleArray } from '$lib/utils';
+  import { page } from '$app/stores';
+  import { get } from 'svelte/store';
 
+  const highlights = get(page).data.highlights;
   // Shuffle the items array and select the first 3 items
-  let featured: AssetFeatured[] = shuffleArray(Web3Featured).slice(0, 3);
+  let featured: AssetFeatured[] = shuffleArray(highlights).slice(0, 3);
 </script>
 
 <FeaturedContainer featuredItems={featured} />
