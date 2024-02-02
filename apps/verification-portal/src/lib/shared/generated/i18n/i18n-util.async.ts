812e5d7d5b61e2875ed78c9b277301425a2fc076
Vadim Smakhtin <vadim@sovereignnature.com>
2024-02-02 14:27:50 +0100

    Moved i18n to generated shared folder

--- apps/verification-portal/src/lib/shared/generated/i18n/i18n-util.async.ts
+++ apps/verification-portal/src/lib/shared/generated/i18n/i18n-util.async.ts
@@ -6,7 +6,6 @@ import type { Locales, Translations } from './i18n-types'
 import { loadedFormatters, loadedLocales, locales } from './i18n-util'
 
 const localeTranslationLoaders = {
-	en: () => import('./en'),
 }
 
 const updateDictionary = (locale: Locales, dictionary: Partial<Translations>): Translations =>
--- apps/verification-portal/src/lib/shared/generated/i18n/i18n-util.ts
+++ apps/verification-portal/src/lib/shared/generated/i18n/i18n-util.ts
@@ -11,7 +11,6 @@ import type { Formatters, Locales, Translations, TranslationFunctions } from './
 export const baseLocale: Locales = 'en'
 
 export const locales: Locales[] = [
-	'en'
 ]
 
 export const isLocale = (locale: string): locale is Locales => locales.includes(locale as Locales)
