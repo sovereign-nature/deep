7c3d7c57a93dcd28ac28dba7699c44a7ad3f5caf
Vadim Smakhtin <vadim@sovereignnature.com>
2024-02-02 12:36:28 +0100

    Fixed Sentry sourcemaps upload (#935)
    * Fixed Sentry sourcemaps upload
    
    * Removed build job from code quality
    
    * Lockfile update

--- apps/verification-portal/vite.config.ts
+++ apps/verification-portal/vite.config.ts
@@ -1,4 +1,5 @@
 // import { sentrySvelteKit } from '@sentry/sveltekit'; //TODO: Return Sentry after sourcemaps upload is fixed
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -10,12 +11,12 @@ const isVisualizeBuild = () => process.env.VITE_VISUALIZE_BUILD === 'true';
 
 export default defineConfig({
   plugins: [
-    // sentrySvelteKit({
-    //   sourceMapsUploadOptions: {
-    //     org: 'sovereign-nature-initiative',
-    //     project: 'real',
-    //   },
-    // }),
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'sovereign-nature-initiative',
+        project: 'real',
+      },
+    }),
     sveltekit(),
     SvelteKitPWA(),
     // Visualize bundle size in stats.html files
