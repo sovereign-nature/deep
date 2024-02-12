291f81732925120724075b666249b5982ea742d0
Vadim Smakhtin <vadim@sovereignnature.com>
2024-02-12 20:14:37 +0100

    Returned vendor split

--- apps/verification-portal/vite.config.ts
+++ apps/verification-portal/vite.config.ts
@@ -2,14 +2,14 @@ import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { visualizer } from 'rollup-plugin-visualizer';
-// import { splitVendorChunkPlugin } from 'vite';
+import { splitVendorChunkPlugin } from 'vite';
 import { defineConfig, type PluginOption } from 'vite';
 
 const isVisualizeBuild = () => process.env.VITE_VISUALIZE_BUILD === 'true';
 
 export default defineConfig({
   plugins: [
-    // splitVendorChunkPlugin(),
+    splitVendorChunkPlugin(), //Not sure it's needed, but otherwise Sentry is broken
     sentrySvelteKit({
       sourceMapsUploadOptions: {
         org: 'sovereign-nature-initiative',
