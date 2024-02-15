cae98fc5f6a907c28b052720528cb020c26a6f96
Vadim Smakhtin <vadim@sovereignnature.com>
2024-02-15 15:27:06 +0100

    Added param validation

--- apps/api/src/routes/assets.ts
+++ apps/api/src/routes/assets.ts
@@ -1,28 +1,40 @@
 import { Hono } from 'hono';
 import { env } from 'hono/adapter';
-
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 import { getAsset, parseDID } from '../lib/shared';
 
 const app = new Hono();
 
-app.get('/:assetDID', async (c) => {
-  const assetDID = c.req.param('assetDID');
+const getAssetRoute = app.get(
+  '/:assetDid',
+  zValidator(
+    'param',
+    z.object({
+      assetDid: z.string(),
+    })
+  ),
+  async (c) => {
+    const assetDID = c.req.param('assetDid');
 
-  const { OPEN_SEA_API_KEY } = env<{ OPEN_SEA_API_KEY: string }>(c);
+    const { OPEN_SEA_API_KEY } = env<{ OPEN_SEA_API_KEY: string }>(c);
 
-  // Parsing DID
-  try {
-    const parsedData = parseDID(assetDID);
-    const assetData = await getAsset(
-      parsedData.networkId,
-      parsedData.assetId,
-      parsedData.tokenId,
-      OPEN_SEA_API_KEY
-    );
-    return c.json(assetData);
-  } catch (e) {
-    return c.json({ error: 'Asset not found' });
+    // Parsing DID
+    try {
+      const parsedData = parseDID(assetDID);
+      const assetData = await getAsset(
+        parsedData.networkId,
+        parsedData.assetId,
+        parsedData.tokenId,
+        OPEN_SEA_API_KEY
+      );
+      return c.json(assetData);
+    } catch (e) {
+      return c.notFound();
+    }
   }
-});
+);
+
+export type GetAssetRoute = typeof getAssetRoute;
 
 export default app;
