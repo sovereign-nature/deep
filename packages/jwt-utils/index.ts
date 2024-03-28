8713b31c1df2cbd199edc79d331d383bf091546a
Vadim Smakhtin <vadim@sovereignnature.com>
2024-03-28 12:00:42 +0100

    Updated jwt-utils

--- packages/jwt-utils/index.ts
+++ packages/jwt-utils/index.ts
@@ -7,3 +7,19 @@ export function encodeJWT(payload: object, secret: Secret): string {
 export function decodeJWT(token: string, secret: Secret) {
   return verify(token, secret);
 }
+
+export function base64EncodeURL(url: string) {
+  const utf8Encode = new TextEncoder();
+  const byteArray = utf8Encode.encode(url);
+
+  return btoa(
+    Array.from(new Uint8Array(byteArray))
+      .map((val) => {
+        return String.fromCharCode(val);
+      })
+      .join('')
+  )
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
+}
