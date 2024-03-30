548f91dd3bb640482237e4efdb8823ede3895510
Maija <hello@maijagrudule.com>
2024-03-30 20:16:26 +0000

    Drawer component init

--- packages/ui-kit/src/lib/index.ts
+++ packages/ui-kit/src/lib/index.ts
@@ -3,5 +3,6 @@ export * as Carousel from '$lib/components/ui/carousel/index.js';
 export * as Card from '$lib/components/ui/card/index.js';
 export * as Toaster from '$lib/components/ui/sonner/index.js';
 export * as MultiTabs from '$lib/components/ui/multi-tabs/index.js';
+export * as Drawer from '$lib/components/ui/drawer/index.js';
 
 export * as Style from './theme.pcss';
