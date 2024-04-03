b665255fe33549753664c3192b88658bdc08a333
Vadim Smakhtin <vadim@sovereignnature.com>
2024-04-03 14:29:39 +0100

    Updated wagmi

--- apps/verification-portal/src/lib/entities/featured/FeaturedContainer.svelte
+++ apps/verification-portal/src/lib/entities/featured/FeaturedContainer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import CardFeatured from '$lib/entities/featured/FeaturedCard.svelte';
   import LL from '$lib/shared/i18n/i18n-svelte';
-  import type { DeepAsset } from '@sni/clients/assets-client/types';
+  import type { DeepAsset } from '@sni/types';
 
   export let featuredItems: DeepAsset[];
   export let collectionName: string | undefined;
@@ -9,15 +9,18 @@
 
 {#if featuredItems && featuredItems.length > 0}
   <div class="mt-8 sm:mt-12">
-    <h2 class="text-gray-200 dark:text-gray-400 text-sm sm:text-xl font-sans sm:font-serif mb-5 text-center md:text-left">
+    <h2
+      class="text-gray-200 dark:text-gray-400 text-sm sm:text-xl font-sans sm:font-serif mb-5 text-center md:text-left"
+    >
       {#if collectionName}
-      {$LL.highlightsFor({collectionName})}
+        {$LL.highlightsFor({ collectionName })}
       {:else}
-      {$LL.highlights()}
+        {$LL.highlights()}
       {/if}
-    
     </h2>
-    <div class="grid justify-center md:justify-start md:grid-cols-2 xl:grid-cols-3 gap-5">
+    <div
+      class="grid justify-center md:justify-start md:grid-cols-2 xl:grid-cols-3 gap-5"
+    >
       {#each featuredItems as item, index (item.address + index)}
         <CardFeatured {item} />
       {/each}
