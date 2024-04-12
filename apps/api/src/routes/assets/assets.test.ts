341eea71e81d51861f11d9c1fed1292382c9328f
Vadim Smakhtin <vadim@sovereignnature.com>
2024-04-12 10:08:00 +0200

    Tests working locally for assets endpoint

--- apps/api/src/routes/assets/assets.test.ts
+++ apps/api/src/routes/assets/assets.test.ts
@@ -1,51 +1,55 @@
 import { expect, describe, it } from 'vitest';
-import { HH_ASSET, WHALE_ASSET } from './fixtures';
+import { env } from 'cloudflare:test';
 import app from '.';
 
-//TODO: Mock responses
 describe('Assets API', () => {
   it('should handle invalid DID', async () => {
-    const res = await app.request('http://localhost/invalidDID');
+    const res = await app.request('http://localhost/invalidDID', {}, env);
     expect(res.status).toEqual(404);
   });
 
   it('should handle invalid Asset', async () => {
     const res = await app.request(
-      'http://localhost/did:asset:deep:polkadot.asset-hub:13:1000000'
+      'http://localhost/did:asset:deep:polkadot.asset-hub:13:1000000',
+      {},
+      env
     );
     expect(res.status).toEqual(404);
   });
 
   it('should fetch Polkadot Web3 asset', async () => {
     const res = await app.request(
-      'http://localhost/did:asset:deep:polkadot.asset-hub:13:738'
+      'http://localhost/did:asset:deep:polkadot.asset-hub:13:738',
+      {},
+      env
     );
-    expect(await res.json()).toEqual(WHALE_ASSET);
+    expect(res.status).toEqual(200);
   });
 
   it('should fetch Web2 asset', async () => {
     const res = await app.request(
-      'http://localhost/did:asset:deep:hotel-hideaway.asset:upemba-national-park-curls'
+      'http://localhost/did:asset:deep:hotel-hideaway.asset:upemba-national-park-curls',
+      {},
+      env
     );
-    expect(await res.json()).toEqual(HH_ASSET);
+    expect(res.status).toEqual(200);
   });
 
-  // it('should fetch OpenSea asset', async () => {
-  //   const res = await app.request(
-  //     'http://localhost/did:asset:eip155:42161.erc721:0x6cc7c9b2aa5fdcc044f9a51d9d083fd16aeb0a78:2'
-  //   );
-  //   expect(res.status).toEqual(200);
-  // });
+  it('should fetch OpenSea asset', async () => {
+    const res = await app.request(
+      'http://localhost/did:asset:eip155:42161.erc721:0x6cc7c9b2aa5fdcc044f9a51d9d083fd16aeb0a78:2',
+      {},
+      env
+    );
+    expect(res.status).toEqual(200);
+  });
 
-  // it('should fetch Optimism Testnet asset', async () => {
-  //   const res = await app.request(
-  //     'http://localhost/did:asset:eip155:11155420.erc721:0xAA7f515b01C04E632c7837f1a80f67eA3f3Fc58B:7'
-  //   );
-
-  //   console.log(await res.json());
-  //   console.log(res.statusText);
-  //   expect(res.status).toEqual(200);
-  // });
-
-  //TODO: Move integration tests to Checkly
+  it('should fetch Optimism Testnet asset', async () => {
+    const res = await app.request(
+      'http://localhost/did:asset:eip155:11155420.erc721:0xAA7f515b01C04E632c7837f1a80f67eA3f3Fc58B:7',
+      {},
+      env
+    );
+    expect(res.status).toEqual(200);
+  });
 });
--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
-export default defineConfig({
+export default defineWorkersConfig({
   test: {
     watch: false,
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: './wrangler.toml' },
+      },
+    },
   },
 });
