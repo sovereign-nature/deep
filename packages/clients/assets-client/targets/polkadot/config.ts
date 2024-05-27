cf9be14fdd44c25030a053abc8b253be720f7cec
Vadim Smakhtin <vadim@sovereignnature.com>
2024-05-27 21:13:48 +0200

    Changed polkadot endpoint

--- packages/clients/assets-client/targets/polkadot/config.ts
+++ packages/clients/assets-client/targets/polkadot/config.ts
@@ -1,2 +1,4 @@
-export const polkadotApiUrl = 'https://squid.subsquid.io/speck/graphql';
+// export const polkadotApiUrl = 'https://squid.subsquid.io/speck/graphql';
+export const polkadotApiUrl =
+  'https://sovereign-nature-initiative.squids.live/speck/v/v12/graphql';
 export const kusamaApiUrl = 'https://squid.subsquid.io/stick/graphql';
