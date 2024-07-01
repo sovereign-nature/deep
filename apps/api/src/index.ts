5ac81ed1924f52a9530bfa1159f453da0efd2197
Vadim Smakhtin <vadim@sovereignnature.com>
2024-07-01 17:32:38 +0200

    Message queue works

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,10 +5,9 @@ import { cache } from 'hono/cache';
 // Importing routes
 import assets from './routes/assets';
 import highlights from './routes/highlights';
-import claims from './routes/claims';
+import claims, { MintRequest } from './routes/claims';
 import wallets from './routes/wallets';
 import events from './routes/events';
-
 const app = new Hono();
 
 app.use('/*', cors({ origin: '*' }));
@@ -29,4 +28,27 @@ app.route('/claims', claims);
 app.route('/wallets', wallets);
 app.route('/events', events);
 
-export default app;
+// export default app;
+
+export default {
+  fetch: app.fetch,
+  async queue(batch: MessageBatch<MintRequest>) {
+    console.log('Processing batch', batch);
+
+    // for (const message of batch.messages) {
+    //   const mintRequest = message.body;
+
+    //   console.log('Processing minting request', mintRequest);
+    // }
+
+    const response = await fetch('https://httpbin.org/delay/40', {
+      headers: { Accept: 'application/json' },
+    });
+
+    const nftResponse = await response.json();
+
+    console.log('NFT Response', nftResponse);
+
+    console.log('Processed batch');
+  },
+};
--- apps/api/src/routes/claims/index.ts
+++ apps/api/src/routes/claims/index.ts
@@ -2,12 +2,19 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { decode, verify } from 'hono/jwt';
 import { env } from 'hono/adapter';
-import { collections } from './config';
+import { CollectionConfig, collections } from './config';
 import { ClaimBody, JWTToken } from './schemas';
 import { mintOptimismToken } from './providers/crossmint';
 import { mintUniqueToken } from './providers/unique';
+import { Payload } from './types';
 
 const app = new Hono();
+
+export type MintRequest = {
+  address: string;
+  payload: Payload;
+  collectionConfig: CollectionConfig;
+};
 
 app.post(
   '/',
@@ -41,5 +48,28 @@ app.post(
     }
   }
 );
+
+app.post('/v2', zValidator('json', ClaimBody), async (c) => {
+  const { CLAIMS_SECRET } = env<{ CLAIMS_SECRET: string }>(c);
+  const { MINTING_QUEUE } = env<{ MINTING_QUEUE: Queue<MintRequest> }>(c);
+
+  const body = ClaimBody.parse(await c.req.json());
+
+  const { token, address } = body;
+
+  try {
+    await verify(token, CLAIMS_SECRET);
+  } catch (e) {
+    return c.json({ error: true, message: 'Invalid token' }, 400);
+  }
+
+  const { payload } = JWTToken.parse(decode(token));
+
+  const collectionConfig = collections[payload.collection];
+
+  MINTING_QUEUE.send({ address, payload, collectionConfig });
+
+  return c.json({ success: true });
+});
 
 export default app;
