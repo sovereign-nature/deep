1d1a5149e27beb2dafbc37fabbb46d22030a35c9
Vadim Smakhtin <vadim@sovereignnature.com>
2024-07-02 12:06:08 +0200

    Added fixtures for testing

---
+++ apps/api/src/routes/claims/providers/fixtures.ts
@@ -0,0 +1,29 @@
+export const OptimismPendingResponse = {
+  id: '8ff3b894-8437-4056-9417-90043c5718de',
+  onChain: {
+    status: 'pending',
+    chain: 'optimism-sepolia',
+    contractAddress: '0xAA7f515b01C04E632c7837f1a80f67eA3f3Fc58B',
+  },
+  actionId: '8ff3b894-8437-4056-9417-90043c5718de',
+};
+
+export const OptimismSuccessResponse = {
+  id: '8ff3b894-8437-4056-9417-90043c5718de',
+  metadata: {
+    name: 'ENGIE Bioacoustics Eco-Badge',
+    description: 'Test collection',
+    image:
+      'ipfs://QmPz44sT22UUtJSrTnzC8mA7znF1vWd5U4ZmzzBnpvdHYE/dakhla_collection_final_85.jpg',
+  },
+  onChain: {
+    status: 'success',
+    chain: 'optimism-sepolia',
+    contractAddress: '0xAA7f515b01C04E632c7837f1a80f67eA3f3Fc58B',
+    owner: '0xB8A976Ad1d87D070b5E5806B98A768B4BB4E4847',
+    tokenId: '74',
+  },
+  actionId: '8ff3b894-8437-4056-9417-90043c5718de',
+  assetDID:
+    'did:asset:eip155:11155420.erc721:0xAA7f515b01C04E632c7837f1a80f67eA3f3Fc58B:74',
+};
