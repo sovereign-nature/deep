5483aa1f9eac3116a7fd997abb5eda7635e606a2
Vadim Smakhtin <vadim@sovereignnature.com>
2024-08-19 18:22:04 +0200

    vitest downgrade

---
+++ apps/api/src/lib/db.test.ts
@@ -0,0 +1,8 @@
+import { expect, describe, it } from 'vitest';
+
+describe('DB Functions', () => {
+  it('should connect to the database', async () => {
+    // Placeholder test
+    expect(true).toEqual(true);
+  });
+});
--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,10 +1,36 @@
-import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
+// import { defineConfig } from 'vitest/config';
+// import tsconfigPaths from 'vite-tsconfig-paths';
+// import path from 'node:path';
+import {
+  defineWorkersProject,
+  // readD1Migrations,
+} from '@cloudflare/vitest-pool-workers/config';
 
-export default defineConfig({
-  test: {
-    watch: false,
-  },
-  //@ts-expect-error - tsconfigPaths is not correctly typed
-  plugins: [tsconfigPaths()],
+// // @ts-expect-error - tsconfigPaths is not correctly typed
+export default defineWorkersProject(async () => {
+  // Read all migrations in the `migrations` directory
+  // const migrationsPath = path.join(__dirname, 'migrations');
+  // const migrations = await readD1Migrations(migrationsPath);
+
+  return {
+    test: {
+      // setupFiles: ['./test/apply-migrations.ts'],
+      poolOptions: {
+        workers: {
+          wrangler: {
+            configPath: './wrangler.toml',
+          },
+        },
+      },
+    },
+    // plugins: [tsconfigPaths()],
+  };
 });
+
+// export default defineConfig({
+//   test: {
+//     watch: false,
+//   },
+
+//   plugins: [tsconfigPaths()],
+// });
