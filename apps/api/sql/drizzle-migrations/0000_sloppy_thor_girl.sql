267c7f2b18720450db9409fb2aad0197658c600f
Vadim Smakhtin <vadim@sovereignnature.com>
2024-08-20 12:46:37 +0200

    Added migrations

---
+++ apps/api/sql/drizzle-migrations/0000_sloppy_thor_girl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `session` (
+	`id` text PRIMARY KEY NOT NULL,
+	`expires_at` integer NOT NULL,
+	`user_id` text,
+	`chain_id` integer,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` text PRIMARY KEY NOT NULL
+);
