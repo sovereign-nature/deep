4ab314f4ffc4f4dfc88d7f280bd7ea030e1f23c4
Vadim Smakhtin <vadim@sovereignnature.com>
2024-08-20 12:37:52 +0200

    Added session schema

--- apps/api/src/lib/db/schemas.ts
+++ apps/api/src/lib/db/schemas.ts
@@ -1,6 +1,13 @@
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('user', {
   id: text('id').notNull().primaryKey(),
   //TODO: Add email, telegram, etc.
+});
+
+export const sessions = sqliteTable('session', {
+  id: text('id').notNull().primaryKey(),
+  expiresAt: integer('expires_at').notNull(),
+  userId: text('user_id').references(() => users.id),
+  chainId: integer('chain_id'),
 });
