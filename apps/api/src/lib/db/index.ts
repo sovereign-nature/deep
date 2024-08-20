75b935125f2b772e9c595458fd23160446aeb3e2
Vadim Smakhtin <vadim@sovereignnature.com>
2024-08-20 13:26:44 +0200

    Added profile

--- apps/api/src/lib/db/index.ts
+++ apps/api/src/lib/db/index.ts
@@ -1,8 +1,25 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
-import { users } from './schemas';
+import { profiles, users } from './schemas';
 
 export async function addUser(db: D1Database, address: string) {
   const orm = drizzle(db);
 
   return orm.insert(users).values({ id: address }).onConflictDoNothing();
+}
+
+export async function setEmail(
+  db: D1Database,
+  address: string,
+  email: string,
+  verified: boolean
+) {
+  const orm = drizzle(db);
+
+  return orm
+    .insert(profiles)
+    .values({ id: address, userId: address, email, emailVerified: verified })
+    .onConflictDoUpdate({
+      target: profiles.id,
+      set: { email, emailVerified: verified },
+    });
 }
--- apps/api/src/lib/db/schemas.ts
+++ apps/api/src/lib/db/schemas.ts
@@ -2,7 +2,6 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('user', {
   id: text('id').notNull().primaryKey(),
-  //TODO: Add email, telegram, etc.
 });
 
 export const sessions = sqliteTable('session', {
@@ -10,4 +9,17 @@ export const sessions = sqliteTable('session', {
   expiresAt: integer('expires_at').notNull(),
   userId: text('user_id').references(() => users.id, { onDelete: 'cascade' }),
   chainId: integer('chain_id'),
+});
+
+export const profiles = sqliteTable('profile', {
+  id: text('id').notNull().primaryKey(),
+  userId: text('user_id').references(() => users.id, { onDelete: 'cascade' }),
+  email: text('email').unique(),
+  emailVerified: integer('email_verified', { mode: 'boolean' })
+    .notNull()
+    .default(false),
+  telegram: text('telegram').unique(),
+  telegramVerified: integer('telegram_verified', { mode: 'boolean' })
+    .notNull()
+    .default(false),
 });
