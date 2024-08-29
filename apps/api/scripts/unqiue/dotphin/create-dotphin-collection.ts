c67e9987ad14cbac3121e136d1c6c6d8a6bb24a8
Vadim Smakhtin <vadim@sovereignnature.com>
2024-08-29 15:50:53 +0200

    Scripts for API collections setup (#1716)
    * Initial scripts structure
    
    * Added dotenv
    
    * Can create a collection
    
    * Added token limits
    
    * Added update script

---
+++ apps/api/scripts/unqiue/dotphin/create-dotphin-collection.ts
@@ -0,0 +1,88 @@
+import { Sr25519Account } from '@unique-nft/utils/sr25519';
+import Sdk from '@unique-nft/sdk';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const NETWORK: 'opal' | 'unique' = 'opal';
+
+// man picture (medium size png)
+const IMAGE_URL =
+  'https://real.myfilebase.com/ipfs/QmVQgYDk7655Tu2nKtbky4pcJV34Kg4NDrVW48jYJZTasC';
+
+const getLinkToCollection = (sdk: Sdk, collectionId: number) => {
+  return `${sdk.options.baseUrl}/collections/v2?collectionId=${collectionId}`;
+};
+
+const createCollection = async (sdk: Sdk): Promise<number> => {
+  const collectionCreationResult = await sdk.collection.createV2({
+    name: 'DOTphin',
+    description: 'Your DOTphin avatar',
+    symbol: 'DOTPHIN',
+    cover_image: { url: IMAGE_URL },
+    limits: { accountTokenOwnershipLimit: 1 }, //TODO: Should we add transfer limit (soul bound)?
+    potential_attributes: [
+      { trait_type: 'level' },
+      { trait_type: 'experience' },
+      { trait_type: 'proofs' },
+      { trait_type: 'name' },
+    ],
+  });
+
+  if (!collectionCreationResult.parsed) {
+    throw collectionCreationResult.error;
+  }
+  const collectionId = collectionCreationResult.parsed.collectionId;
+
+  console.log(
+    `Collection created, id ${collectionId}. ${getLinkToCollection(
+      sdk,
+      collectionId
+    )}`
+  );
+
+  return collectionId;
+};
+
+const main = async () => {
+  // init substrate account and sdk
+  const mnemonic = process.env.WALLET_MNEMONIC;
+  if (!mnemonic) throw new Error('SUBSTRATE_MNEMONIC env variable is not set');
+  const account = Sr25519Account.fromUri(mnemonic);
+
+  console.log('Account address:', account.address);
+
+  const sdk = new Sdk({
+    baseUrl: `https://rest.unique.network/${NETWORK}/v1`,
+    account,
+    waitBetweenStatusRequestsInMs: 5000,
+  });
+
+  const collectionId = await createCollection(sdk);
+  console.log('Collection created:', collectionId);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    if (typeof error === 'object' && error !== null) {
+      if (error.isAxiosError === true) {
+        const url =
+          error.response?.request?.res?.responseUrl || error.config?.url;
+        console.log({ ...error.response?.data, url });
+        if (error.details) {
+          console.dir(error.details, { depth: 100 });
+        }
+      } else {
+        if (error.details) {
+          console.log(error.toString());
+          console.dir(error.details, { depth: 100 });
+        } else {
+          console.error(error);
+        }
+      }
+    } else {
+      console.error(error);
+    }
+    process.exit(1);
+  });
---
+++ apps/api/scripts/unqiue/dotphin/mint-token.ts
@@ -0,0 +1,96 @@
+import { Sr25519Account } from '@unique-nft/utils/sr25519';
+import Sdk from '@unique-nft/sdk';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const NETWORK: 'opal' | 'unique' = 'opal';
+
+// man picture (medium size png)
+const IMAGE_URL =
+  'https://real.myfilebase.com/ipfs/QmVQgYDk7655Tu2nKtbky4pcJV34Kg4NDrVW48jYJZTasC';
+
+const COLLECTION_ID = 3707;
+
+const getLinkToToken = (sdk: Sdk, collectionId: number, tokenId: number) => {
+  return `${sdk.options.baseUrl}/tokens/v2?collectionId=${collectionId}&tokenId=${tokenId}`;
+};
+
+const mintTokens = async (sdk: Sdk, collectionId: number) => {
+  const tokensMintingResult = await sdk.token.createMultipleV2({
+    collectionId,
+    tokens: [
+      {
+        owner: '0xB8A976Ad1d87D070b5E5806B98A768B4BB4E4849',
+        name: 'DOTphin #1',
+        image: IMAGE_URL,
+        attributes: [
+          { trait_type: 'level', value: 1 },
+          { trait_type: 'experience', value: 0 },
+          {
+            trait_type: 'proofs',
+            value: 'did:asset:eip155:8880.unique2:665:12',
+          },
+          { trait_type: 'name', value: 'DOTphin' },
+        ],
+      },
+    ],
+  });
+  if (!tokensMintingResult.parsed) {
+    throw tokensMintingResult.error;
+  }
+  const tokenIds = tokensMintingResult.parsed.map(({ tokenId }) => tokenId);
+
+  console.log(
+    `Tokens minted in collection ${collectionId}, ids ${tokenIds.join(', ')}`
+  );
+  for (const tokenId of tokenIds) {
+    console.log(`${getLinkToToken(sdk, collectionId, tokenId)}`);
+  }
+
+  return tokenIds;
+};
+
+const main = async () => {
+  // init substrate account and sdk
+  const mnemonic = process.env.WALLET_MNEMONIC;
+  if (!mnemonic) throw new Error('SUBSTRATE_MNEMONIC env variable is not set');
+  const account = Sr25519Account.fromUri(mnemonic);
+
+  console.log('Account address:', account.address);
+
+  const sdk = new Sdk({
+    baseUrl: `https://rest.unique.network/${NETWORK}/v1`,
+    account,
+    waitBetweenStatusRequestsInMs: 5000,
+  });
+
+  const tokenIds = await mintTokens(sdk, COLLECTION_ID);
+
+  console.log('Tokens minted:', tokenIds);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    if (typeof error === 'object' && error !== null) {
+      if (error.isAxiosError === true) {
+        const url =
+          error.response?.request?.res?.responseUrl || error.config?.url;
+        console.log({ ...error.response?.data, url });
+        if (error.details) {
+          console.dir(error.details, { depth: 100 });
+        }
+      } else {
+        if (error.details) {
+          console.log(error.toString());
+          console.dir(error.details, { depth: 100 });
+        } else {
+          console.error(error);
+        }
+      }
+    } else {
+      console.error(error);
+    }
+    process.exit(1);
+  });
