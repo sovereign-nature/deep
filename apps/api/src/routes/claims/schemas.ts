39218614ad767426d850fcd005ce65838ac85e25
Vadim Smakhtin <vadim@sovereignnature.com>
2024-08-29 13:11:07 +0200

    Moved crossmint schema

--- apps/api/src/routes/claims/schemas.ts
+++ apps/api/src/routes/claims/schemas.ts
@@ -17,23 +17,3 @@ export const JWTToken = z.object({
     seed: z.number(),
   }),
 });
-
-//TODO: Split schema into pending and completed responses
-export const CrossmintResponse = z.object({
-  id: z.string(),
-  metadata: z
-    .object({
-      name: z.string().optional(),
-      description: z.string().optional(),
-      image: z.string(),
-    })
-    .optional(),
-  onChain: z.object({
-    status: z.string(), //TODO: Enum
-    chain: z.string(),
-    contractAddress: z.string(),
-    owner: z.string().optional(),
-    tokenId: z.string().optional(),
-  }),
-  actionId: z.string(),
-});
