3f26f7e38b5eafcfc0928cd3245500e8c70901b1
Vadim Smakhtin <vadim@sovereignnature.com>
2024-09-09 16:41:12 +0200

    Moved uniqueSDK constructor to lib

---
+++ apps/api/src/lib/unique.ts
@@ -0,0 +1,14 @@
+import Sdk from '@unique-nft/sdk';
+import { Sr25519Account } from '@unique-nft/utils/sr25519';
+
+export function getUniqueSdk(mnemonic: string, network: 'opal' | 'unique') {
+  const account = Sr25519Account.fromUri(mnemonic);
+
+  const sdk = new Sdk({
+    baseUrl: `https://rest.unique.network/${network}/v1`,
+    account,
+    axiosConfig: { adapter: 'fetch' },
+  });
+
+  return sdk;
+}
