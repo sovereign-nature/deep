3f393c0e795fc4ac98a9d1b54b238d6395d3ef22
Maija <hello@maijagrudule.com>
2024-09-11 14:57:00 +0300

    Added loading state to web3connect btn

--- apps/verification-portal/src/lib/widgets/ButtonWalletConnect/Web3ConnectBtn.svelte
+++ apps/verification-portal/src/lib/widgets/ButtonWalletConnect/Web3ConnectBtn.svelte
@@ -5,6 +5,7 @@
   import ConnectIcon from '$lib/components/icons/ConnectIcon.svelte';
   import { shortenAddress } from '$lib/shared/utils';
   import { getWeb3Modal } from '$lib/features/web3Modal';
+  import Spinner from '$lib/components/icons/Spinner.svelte';
 
   let isLoaded = false;
   export let alwaysOpen = false;
@@ -57,4 +58,10 @@
       <ConnectIcon slot="icon" className="h-4 w-4 mx-1" />
     </RolloverBtn>
   {/if}
+{:else}
+  <div
+    class={`${responsive ? 'h-8 md:h-11  px-3 md:px-5' : ' h-11 px-5'} flex items-center justify-center `}
+  >
+    <Spinner className="text-primary-400 fill-primary-100 h-4" />
+  </div>
 {/if}
