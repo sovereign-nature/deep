9b9de354a500b667ee5d4d77e725d013218d7811
Vadim Smakhtin <vadim@sovereignnature.com>
2024-09-16 13:44:56 +0200

    Added random images for DOTphin

--- apps/api/src/routes/dotphin/config.ts
+++ apps/api/src/routes/dotphin/config.ts
@@ -1,4 +1,5 @@
 import { CollectionConfig } from '../claims/config'; //TODO: Move to shared
+import { getRandomInt } from '$lib/utils';
 
 const baseAttributes = [
   { trait_type: 'level', value: '1' },
@@ -6,9 +7,28 @@ const baseAttributes = [
   { trait_type: 'type', value: 'orbo' },
 ];
 
+const images = {
+  air: [
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/air/dotphin-orbo-air-0.png',
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/air/dotphin-orbo-air-1.png',
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/air/dotphin-orbo-air-2.png',
+  ],
+  earth: [
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/earth/dotphin-orbo-earth-0.png',
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/earth/dotphin-orbo-earth-1.png',
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/earth/dotphin-orbo-earth-2.png',
+  ],
+  water: [
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/water/dotphin-orbo-water-0.png',
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/water/dotphin-orbo-water-1.png',
+    'https://real.myfilebase.com/ipfs/QmbCKLNSdDUia33AQ8FvGc7UhrGsTuVbhnyPsPKfWUoexu/water/dotphin-orbo-water-2.png',
+  ],
+};
+
 export function getDotphinCollectionConfig(
   collectionId: string
 ): CollectionConfig {
+  const imageSeed = getRandomInt(0, 2);
   return {
     name: 'DOTphin',
     externalId: collectionId,
@@ -18,9 +38,9 @@ export function getDotphinCollectionConfig(
       description:
         "Beneath the depths of the Polkadot ecosystem, something extraordinary lurks - the enigmatic DOTphin. This mystical being embodies the essence of nature, continuously growing and transforming. The adventure commences with Orbo, a powerful symbol of life's boundless potential. Orbo serves as a blank canvas from which all aspects of existence emerge, a primal Orb pulsating with promise. It signifies the genesis of life's endless opportunities.",
       image: [
-        'https://real.myfilebase.com/ipfs/QmSd8qhWTqYvPYGEnXcB9cRhFGwA8XVR1wfN5GGv4x4vJj/orbo-air.png',
-        'https://real.myfilebase.com/ipfs/QmSd8qhWTqYvPYGEnXcB9cRhFGwA8XVR1wfN5GGv4x4vJj/orbo-earth.png',
-        'https://real.myfilebase.com/ipfs/QmSd8qhWTqYvPYGEnXcB9cRhFGwA8XVR1wfN5GGv4x4vJj/orbo-water.png',
+        images.air[imageSeed],
+        images.earth[imageSeed],
+        images.water[imageSeed],
       ],
       attributes: [
         [{ trait_type: 'mainElement', value: 'air' }, ...baseAttributes],
