151a8129365ca16d09a0f9d4daaac91407d60999
Vadim Smakhtin <vadim@sovereignnature.com>
2024-10-07 13:22:01 +0200

    Added merge madrid event

--- apps/api/src/routes/events/config.ts
+++ apps/api/src/routes/events/config.ts
@@ -60,4 +60,10 @@ export const events: Events = {
     seed: () => getRandomInt(0, 2),
     going: true,
   },
+  'merge-madrid-2024': {
+    collectionId: 'merge-madrid-2024',
+    realCollection: 'dotphin-proofs',
+    seed: () => getRandomInt(0, 2),
+    going: false,
+  },
 };
