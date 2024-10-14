10e79120ab8f50a1026e05415094212fe2a5f954
Vadim Smakhtin <vadim@sovereignnature.com>
2024-10-14 14:18:28 +0200

    Swapped minting KV with DB

--- apps/api/src/lib/db/mints.ts
+++ apps/api/src/lib/db/mints.ts
@@ -22,3 +22,13 @@ export async function getMint(db: D1Database, id: string) {
   const result = await orm.select().from(mints).where(eq(mints.id, id));
   return result[0];
 }
+
+export async function updateMint(
+  db: D1Database,
+  id: string,
+  tokenData: CrossmintResponse
+) {
+  const orm = drizzle(db);
+
+  return orm.update(mints).set({ tokenData }).where(eq(mints.id, id));
+}
